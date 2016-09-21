7851e869bb94f8d9e43986ad5b13b1108eff38dc
Fabian Reinartz <fab.reinartz@gmail.com>
2016-09-22 00:45:31 +0200

    Add controller loop and deploy Prometheus server
    This adds the basic controller loop listening for events on the
    prometheus.prometheus.coreos.com resource. It creates a Prometheus
    service and replica set for new Prometheus deployments.

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,18 +1,29 @@
 package controller
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"net/url"
 	"os"
 
+	"github.com/coreos/kube-prometheus-controller/pkg/prometheus"
+
 	"github.com/go-kit/kit/log"
 	"k8s.io/client-go/1.4/kubernetes"
+	apierrors "k8s.io/client-go/1.4/pkg/api/errors"
+	"k8s.io/client-go/1.4/pkg/api/unversioned"
+	api "k8s.io/client-go/1.4/pkg/api/v1"
+	extensionsobj "k8s.io/client-go/1.4/pkg/apis/extensions/v1beta1"
+	"k8s.io/client-go/1.4/pkg/watch"
 	"k8s.io/client-go/1.4/rest"
 )
 
 const (
-	tprMonitor    = "monitor.prometheus.coreos.com"
-	tprPrometheus = "prometheus.prometheus.coreos.com"
+	tprServiceMonitor = "service-monitor.prometheus.coreos.com"
+	tprPrometheus     = "prometheus.prometheus.coreos.com"
 )
 
 // Controller manages lify cycle of Prometheus deployments and
@@ -20,15 +31,101 @@ const (
 type Controller struct {
 	kclient *kubernetes.Clientset
 	logger  log.Logger
+	host    string
 }
 
+// Config defines configuration parameters for the Controller.
 type Config struct {
 	Host        string
 	TLSInsecure bool
 	TLSConfig   rest.TLSClientConfig
 }
 
-func newClient(host string, tlsInsecure bool, tlsConfig *rest.TLSClientConfig) (*kubernetes.Clientset, error) {
+// New creates a new controller.
+func New(c Config) (*Controller, error) {
+	cfg, err := newClusterConfig(c.Host, c.TLSInsecure, &c.TLSConfig)
+	if err != nil {
+		return nil, err
+	}
+	client, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+	logger := log.NewContext(log.NewLogfmtLogger(os.Stdout)).
+		With("ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+
+	return &Controller{
+		kclient: client,
+		logger:  logger,
+		host:    cfg.Host,
+	}, nil
+}
+
+// Run the controller.
+func (c *Controller) Run() error {
+	v, err := c.kclient.Discovery().ServerVersion()
+	if err != nil {
+		return fmt.Errorf("communicating with server failed: %s", err)
+	}
+	c.logger.Log("msg", "connection established", "cluster-version", v)
+
+	if err := c.createTPRs(); err != nil {
+		return err
+	}
+
+	events, errc := c.monitorPrometheusServers(c.kclient.CoreClient.Client, "0")
+	for {
+		select {
+		case evt := <-events:
+			switch evt.Type {
+			case watch.Added:
+				prometheus.New(c.kclient, evt.Object)
+			case watch.Modified:
+				c.logger.Log("msg", "updated event received", "prometheus", evt.Object.Name)
+			case watch.Deleted:
+				c.logger.Log("msg", "delete event received", "prometheus", evt.Object.Name)
+			default:
+				c.logger.Log("msg", "unknown event type received", "type", evt.Type)
+			}
+		case err := <-errc:
+			c.logger.Log("msg", "received error on Prometheus watch", "err", err)
+		}
+	}
+}
+
+func (c *Controller) createTPRs() error {
+	tprs := []*extensionsobj.ThirdPartyResource{
+		{
+			ObjectMeta: api.ObjectMeta{
+				Name: tprServiceMonitor,
+			},
+			Versions: []extensionsobj.APIVersion{
+				{Name: "v1"},
+			},
+			Description: "Prometheus monitoring for a service",
+		},
+		{
+			ObjectMeta: api.ObjectMeta{
+				Name: tprPrometheus,
+			},
+			Versions: []extensionsobj.APIVersion{
+				{Name: "v1"},
+			},
+			Description: "Managed Prometheus server",
+		},
+	}
+	tprClient := c.kclient.Extensions().ThirdPartyResources()
+
+	for _, tpr := range tprs {
+		if _, err := tprClient.Create(tpr); err != nil && !apierrors.IsAlreadyExists(err) {
+			return err
+		}
+		c.logger.Log("msg", "TPR created", "tpr", tpr.Name)
+	}
+	return nil
+}
+
+func newClusterConfig(host string, tlsInsecure bool, tlsConfig *rest.TLSClientConfig) (*rest.Config, error) {
 	var cfg *rest.Config
 	var err error
 
@@ -52,33 +149,63 @@ func newClient(host string, tlsInsecure bool, tlsConfig *rest.TLSClientConfig) (
 	cfg.QPS = 100
 	cfg.Burst = 100
 
-	c, err := kubernetes.NewForConfig(cfg)
-	if err != nil {
-		return nil, err
-	}
-	return c, nil
+	return cfg, nil
 }
 
-// New creates a new controller.
-func New(c Config) (*Controller, error) {
-	client, err := newClient(c.Host, c.TLSInsecure, &c.TLSConfig)
-	if err != nil {
-		return nil, err
-	}
-	return &Controller{
-		kclient: client,
-		logger:  log.NewLogfmtLogger(os.Stdout),
-	}, nil
+// Event represents an event in the cluster.
+type Event struct {
+	Type   watch.EventType
+	Object *prometheus.Object
 }
 
-// Run the controller.
-func (c *Controller) Run() error {
-	v, err := c.kclient.Discovery().ServerVersion()
-	if err != nil {
-		return fmt.Errorf("communicating with server failed: %s", err)
-	}
-	c.logger.Log("msg", "connection established", "version", v)
+func (c *Controller) monitorPrometheusServers(client *http.Client, watchVersion string) (<-chan *Event, <-chan error) {
+	var (
+		events = make(chan *Event)
+		errc   = make(chan error, 1)
+	)
+	go func() {
+		for {
+			resp, err := client.Get(c.host + "/apis/prometheus.coreos.com/v1/namespaces/default/prometheuses?watch=true&resourceVersion=" + watchVersion)
+			if err != nil {
+				errc <- err
+				return
+			}
+			if resp.StatusCode != 200 {
+				resp.Body.Close()
+				errc <- errors.New("Invalid status code: " + resp.Status)
+				return
+			}
+			c.logger.Log("msg", "watching Prometheus resource", "version", watchVersion)
 
-	select {}
-	panic("unreachable")
+			for {
+
+				dec := json.NewDecoder(resp.Body)
+				var evt Event
+				if err := dec.Decode(&evt); err != nil {
+					if err == io.EOF {
+						break
+					}
+					c.logger.Log("msg", "failed to get event from apiserver", "err", err)
+					errc <- err
+					break
+				}
+				if evt.Type == "ERROR" {
+					break
+				}
+				c.logger.Log("msg", "Prometheus event", "type", evt.Type, "obj", fmt.Sprintf("%v", evt.Object))
+				watchVersion = evt.Object.ObjectMeta.ResourceVersion
+				events <- &evt
+			}
+		}
+	}()
+	return events, errc
+}
+
+type PrometheusList struct {
+	unversioned.TypeMeta `json:",inline"`
+	// Standard list metadata
+	// More info: http://releases.k8s.io/HEAD/docs/devel/api-conventions.md#metadata
+	unversioned.ListMeta `json:"metadata,omitempty"`
+	// Items is a list of third party objects
+	Items []prometheus.Prometheus `json:"items"`
 }
---
+++ pkg/prometheus/prometheus.go
@@ -0,0 +1,137 @@
+package prometheus
+
+import (
+	"fmt"
+
+	"k8s.io/client-go/1.4/kubernetes"
+	core "k8s.io/client-go/1.4/kubernetes/typed/core/v1"
+	extensions "k8s.io/client-go/1.4/kubernetes/typed/extensions/v1beta1"
+	apierrors "k8s.io/client-go/1.4/pkg/api/errors"
+	unversionedapi "k8s.io/client-go/1.4/pkg/api/unversioned"
+	apiv1 "k8s.io/client-go/1.4/pkg/api/v1"
+	extensionsapi "k8s.io/client-go/1.4/pkg/apis/extensions/v1beta1"
+	"k8s.io/client-go/1.4/pkg/util/intstr"
+)
+
+// Object represents an Prometheus TPR API object.
+type Object struct {
+	unversionedapi.TypeMeta `json:",inline"`
+	apiv1.ObjectMeta        `json:"metadata,omitempty"`
+	Spec                    Spec `json:"spec"`
+}
+
+// Spec defines a Prometheus server.
+type Spec struct {
+	ServiceMonitors []SpecServiceMonitor `json:"serviceMonitors"`
+}
+
+// SpecServiceMonitor references a service monitor belonging to a Prometheus server.
+type SpecServiceMonitor struct {
+	Name string `json:"name"`
+}
+
+// Prometheus manages the life-cycle of a single Prometheus server
+// in the cluster.
+type Prometheus struct {
+	*Object
+
+	kclient *kubernetes.Clientset
+	errc    chan<- error
+}
+
+// New returns a new Prometheus server manager for a newly created Prometheus.
+func New(kc *kubernetes.Clientset, o *Object) (*Prometheus, <-chan error) {
+	errc := make(chan error)
+	p := &Prometheus{
+		Object:  o,
+		kclient: kc,
+		errc:    errc,
+	}
+
+	go p.run()
+	return p, errc
+}
+
+// Delete romves the Prometheus server deployment.
+func (p *Prometheus) Delete() error {
+	return fmt.Errorf("not implemented")
+}
+
+func (p *Prometheus) run() {
+	if err := p.create(); err != nil {
+		p.errc <- err
+	}
+}
+
+func (p *Prometheus) create() error {
+	if err := createService(p.kclient.Core().Services(p.Namespace), p.Name); err != nil {
+		return err
+	}
+	if err := createReplicaSet(p.kclient.ExtensionsClient.ReplicaSets(p.Namespace), p.Name); err != nil {
+		return err
+	}
+	return nil
+}
+
+func createReplicaSet(client extensions.ReplicaSetInterface, name string) error {
+	replicas := int32(1)
+	rs := &extensionsapi.ReplicaSet{
+		ObjectMeta: apiv1.ObjectMeta{
+			Name: name,
+		},
+		Spec: extensionsapi.ReplicaSetSpec{
+			Replicas: &replicas,
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: apiv1.ObjectMeta{
+					Labels: map[string]string{
+						"prometheus.coreos.com": name,
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:  "prometheus",
+							Image: "quay.io/prometheus/prometheus:latest",
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "web",
+									ContainerPort: 9090,
+									Protocol:      apiv1.ProtocolTCP,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	if _, err := client.Create(rs); err != nil && !apierrors.IsAlreadyExists(err) {
+		return err
+	}
+	return nil
+}
+
+func createService(client core.ServiceInterface, name string) error {
+	svc := &apiv1.Service{
+		ObjectMeta: apiv1.ObjectMeta{
+			Name: name,
+		},
+		Spec: apiv1.ServiceSpec{
+			Ports: []apiv1.ServicePort{
+				{
+					Name:       "web",
+					Port:       9090,
+					TargetPort: intstr.FromString("web"),
+					Protocol:   apiv1.ProtocolTCP,
+				},
+			},
+			Selector: map[string]string{
+				"prometheus.coreos.com": name,
+			},
+		},
+	}
+	if _, err := client.Create(svc); err != nil && !apierrors.IsAlreadyExists(err) {
+		return err
+	}
+	return nil
+}
