cc74f514863472bb89f50a9fbd9bf5620c44ad52
Frederic Branczyk <fbranczyk@gmail.com>
2017-01-02 16:08:12 +0100

    pkg/alertmanager: add ability to specify -web.external-url

--- pkg/spec/spec.go
+++ pkg/spec/spec.go
@@ -144,6 +144,13 @@ type AlertmanagerSpec struct {
 	// Storage is the definition of how storage will be used by the Alertmanager
 	// instances.
 	Storage *StorageSpec `json:"storage"`
+	// ExternalURL is the URL under which Alertmanager is externally reachable
+	// (for example, if Alertmanager is served via a reverse proxy). Used for
+	// generating relative and absolute links back to Alertmanager itself. If the
+	// URL has a path portion, it will be used to prefix all HTTP endpoints
+	// served by Alertmanager. If omitted, relevant URL components will be
+	// derived automatically.
+	ExternalURL string `json:"externalUrl,omitempty"`
 }
 
 type AlertmanagerList struct {
