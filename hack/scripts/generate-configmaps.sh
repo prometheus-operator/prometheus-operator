9e11e17a710bc006cdc04cc35d9ca7f36babfa79
Frederic Branczyk <fbranczyk@gmail.com>
2017-03-03 16:43:52 +0100

    *: normalize namings

--- hack/scripts/generate-configmaps.sh
+++ hack/scripts/generate-configmaps.sh
@@ -4,5 +4,5 @@
 kubectl create configmap --dry-run=true prometheus-k8s-rules --from-file=assets/prometheus/rules/ -oyaml > manifests/prometheus/prometheus-k8s-rules.yaml
 
 # Generate Dashboard ConfigMap
-kubectl create configmap --dry-run=true grafana-dashboards --from-file=assets/grafana/ -oyaml > manifests/grafana/grafana-cm.yaml
+kubectl create configmap --dry-run=true grafana-dashboards --from-file=assets/grafana/ -oyaml > manifests/grafana/grafana-dashboards.yaml
 
