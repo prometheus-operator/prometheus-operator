3f6f7a89a101e6f1ae4fced902455fc10e37521f
Frederic Branczyk <fbranczyk@gmail.com>
2017-03-06 10:30:36 +0100

    contrib/grafana-watcher: add license headers

--- scripts/check_license.sh
+++ scripts/check_license.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 licRes=$(
-for file in $(find . -type f -iname '*.go' ! -path './vendor/*'); do
+for file in $(find . -type f -iname '*.go' ! -path '*/vendor/*'); do
 	head -n3 "${file}" | grep -Eq "(Copyright|generated|GENERATED)" || echo -e "  ${file}"
 done;)
 if [ -n "${licRes}" ]; then
