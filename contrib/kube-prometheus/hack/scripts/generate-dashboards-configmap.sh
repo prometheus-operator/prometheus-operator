574d58800724db77c4662b27693920779e5cbdb9
Frederic Branczyk <fbranczyk@gmail.com>
2017-03-13 12:08:30 +0100

    kube-prometheus: remove duplication in grafana dashboards
    Datasource links were duplicated in the grafana dashboads. This now also
    allows exporting grafana dashboards from the UI and just dropping them
    into the assets directory and they will be wrapped by the manifest
    generation script.

--- contrib/kube-prometheus/hack/scripts/generate-dashboards-configmap.sh
+++ contrib/kube-prometheus/hack/scripts/generate-dashboards-configmap.sh
@@ -8,7 +8,13 @@ metadata:
 data:
 EOF
 
-for f in assets/grafana/*
+for f in assets/grafana/*-dashboard.json
+do
+  echo "  $(basename $f): |+"
+  hack/scripts/wrap-dashboard.sh $f | sed "s/^/    /g"
+done
+
+for f in assets/grafana/*-datasource.json
 do
   echo "  $(basename $f): |+"
   cat $f | sed "s/^/    /g"
