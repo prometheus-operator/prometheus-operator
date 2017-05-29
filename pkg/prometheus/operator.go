09c57a17d1e2332a37a9a81c2eb15add91bab26c
Frederic Branczyk <fbranczyk@gmail.com>
2017-05-29 09:59:55 +0200

    *: add metrics

--- pkg/prometheus/operator.go
+++ pkg/prometheus/operator.go
@@ -28,6 +28,7 @@ import (
 	"github.com/coreos/prometheus-operator/third_party/workqueue"
 	"github.com/go-kit/kit/log"
 	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -199,6 +200,10 @@ func New(conf Config, logger log.Logger) (*Operator, error) {
 	}
 
 	return c, nil
+}
+
+func (c *Operator) RegisterMetrics(r prometheus.Registerer) {
+	r.MustRegister(NewPrometheusCollector(c.promInf.GetStore()))
 }
 
 // Run the controller.
