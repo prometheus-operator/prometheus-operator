8f0e4e1500af46fea2f84b6539caae20481e8392
Tao Lu <tao-lu@users.noreply.github.com>
2017-08-02 12:20:19 -0700

    Update promcfg.go

--- pkg/prometheus/promcfg.go
+++ pkg/prometheus/promcfg.go
@@ -153,6 +153,9 @@ func generateServiceMonitorConfig(version semver.Version, m *v1alpha1.ServiceMon
 	if ep.Interval != "" {
 		cfg = append(cfg, yaml.MapItem{Key: "scrape_interval", Value: ep.Interval})
 	}
+	if ep.Timeout != "" {
+		cfg = append(cfg, yaml.MapItem(Key: "scrape_timeout", Value: ep.Timeout})
+	}
 	if ep.Path != "" {
 		cfg = append(cfg, yaml.MapItem{Key: "metrics_path", Value: ep.Path})
 	}
