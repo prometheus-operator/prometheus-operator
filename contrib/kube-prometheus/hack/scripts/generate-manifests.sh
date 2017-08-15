f39db39de76ba9bade945c3aa11d5ab44b3aa78c
Eduardo Gonzalez <eduardo.gonzalez.de.la.herran@ericsson.com>
2017-08-15 17:18:09 +0200

    grafana configmap generator integrated

--- contrib/kube-prometheus/hack/scripts/generate-manifests.sh
+++ contrib/kube-prometheus/hack/scripts/generate-manifests.sh
@@ -4,7 +4,14 @@
 hack/scripts/generate-rules-configmap.sh > manifests/prometheus/prometheus-k8s-rules.yaml
 
 # Generate Dashboard ConfigMap
-hack/scripts/generate-dashboards-configmap.sh > manifests/grafana/grafana-dashboards.yaml
+#hack/scripts/generate-dashboards-configmap.sh > manifests/grafana/grafana-dashboards.yaml
+
+# Generate Dashboard ConfigMap with configmap-generator tool
+# Max Size per ConfigMap: 240000
+# Input dir: assets/grafana
+# output file: manifests/grafana/grafana-dashboards.yaml
+test -f manifests/grafana/grafana-dashboards.yaml && rm -f manifests/grafana/grafana-dashboards.yaml
+hack/grafana-dashboards-configmap-generator/bin/grafana_dashboards_generate.sh -s 240000 -i assets/grafana -o manifests/grafana/grafana-dashboards.yaml
 
 # Generate Grafana Credentials Secret
 hack/scripts/generate-grafana-credentials-secret.sh admin admin > manifests/grafana/grafana-credentials.yaml
