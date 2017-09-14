5ecffe9c90416ccfe2c0ff2835f102bc8455b54a
Kevin Jing Qiu <kevin@idempotent.ca>
2017-09-14 11:41:33 -0400

    Some more tests

--- pkg/alertrule/operator_test.go
+++ pkg/alertrule/operator_test.go
@@ -6,6 +6,22 @@ func TestAlertruleKeyToConfigMapKey(t *testing.T) {
 	expected := "default/alertrule-node-exporter-down"
 	cfgMapKey := alertruleKeyToConfigMapKey("default/node-exporter-down")
 	if cfgMapKey != expected {
-		t.Fatalf("Expected: %v, Actual %v", expected, cfgMapKey)
+		t.Fatalf("Expected: %s, Actual %s", expected, cfgMapKey)
+	}
+}
+
+func TestConfigMapKeyToAlertruleKey(t *testing.T) {
+	expected := "default/danger-danger"
+	arKey := configMapKeyToAlertruleKey("default/alertrule-danger-danger")
+	if arKey != expected {
+		t.Fatalf("Expected: %s, Actual %s", expected, arKey)
+	}
+}
+
+func TestAlertruleNameToAlertrulePath(t *testing.T) {
+	expected := "danger-danger.rules"
+	path := alertruleNameToAlertrulePath("danger-danger")
+	if path != expected {
+		t.Fatalf("Expected: %s, Actual %s", expected, path)
 	}
 }
