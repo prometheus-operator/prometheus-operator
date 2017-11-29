4dd3ce62186f8402d9f6324f08acc08cc728d752
Giancarlo Rubio <gianrubio@gmail.com>
2017-11-29 09:57:00 +0000

    Enable e2e tests on helm charts

--- scripts/travis-e2e.sh
+++ scripts/travis-e2e.sh
@@ -8,36 +8,12 @@ set -u
 # print each command before executing it
 set -x
 
-export MINIKUBE_VERSION=v0.24.1
-export KUBERNETES_VERSION=v1.8.0
-
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_VERSION/bin/linux/amd64/kubectl && \
-    chmod +x kubectl &&  \
-    sudo mv kubectl /usr/local/bin/
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/$MINIKUBE_VERSION/minikube-linux-amd64 && \
-    chmod +x minikube && \
-    sudo mv minikube /usr/local/bin/
-
-export MINIKUBE_WANTUPDATENOTIFICATION=false
-export MINIKUBE_WANTREPORTERRORPROMPT=false
-export MINIKUBE_HOME=$HOME
-export CHANGE_MINIKUBE_NONE_USER=true
-mkdir $HOME/.kube || true
-touch $HOME/.kube/config
-
-export KUBECONFIG=$HOME/.kube/config
-minikube version
-sudo minikube start --vm-driver=none --kubernetes-version=$KUBERNETES_VERSION --extra-config=apiserver.Authorization.Mode=RBAC
-
-minikube update-context
-
-# waiting for node(s) to be ready
-JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
-
-kubectl apply -f scripts/minikube-rbac.yaml
+$(dirname "$BASH_SOURCE")/create-minikube.sh
 
 # waiting for kube-dns to be ready
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl -n kube-system get pods -lk8s-app=kube-dns -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1;echo "waiting for kube-dns to be available"; kubectl get pods --all-namespaces; done
 
 make crossbuild
 make e2e
+
+$(dirname "$BASH_SOURCE")/delete-minikube.sh
