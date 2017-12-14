8ba566f05c664fc00b5e45f35ad4ae77dfd240d0
Giancarlo Rubio <gianrubio@gmail.com>
2017-12-14 22:09:38 +0100

    Do not stop the helm sync when the hash comparison fail

--- helm/hack/sync-repo.sh
+++ helm/hack/sync-repo.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
-set -o xtrace
 
 HELM_BUCKET_NAME="coreos-charts"
 HELM_CHARTS_PACKAGED_DIR=${1:-"/tmp/helm-packaged"}
@@ -18,8 +14,7 @@ do
   remote_hash=$(aws s3api head-object --bucket ${HELM_BUCKET_NAME} --key stable/${tgz} | jq '.ETag' -r| tr -d '"')
   if [ "$cur_hash" != "$remote_hash" ]
   then
-    echo "Current hash should be the same as the remote hash. Please bump the version of chart {$tgz}."
-    exit 1
+    echo "WARNING: Current hash should be the same as the remote hash. Please bump the version of chart {$tgz}."
    fi
 done
 
