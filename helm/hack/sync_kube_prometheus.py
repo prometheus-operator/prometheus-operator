d9682e297a1ac590f154f28f996eb3271ac9f25f
Vladimir Glafirov <vglafirov@gmail.com>
2018-02-25 21:53:35 +0100

    removed dashboard generation

--- helm/hack/sync_kube_prometheus.py
+++ helm/hack/sync_kube_prometheus.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import os
 import re
-from ruamel import yaml 
-  
+from ruamel import yaml
+
 def escape(s):
   return s.replace("{{","{{`{{").replace("}}","}}`}}")
-  
+
 def get_header(file_name):
-  return "{{ define \"" + file_name + ".tpl\" }}\n" 
+  return "{{ define \"" + file_name + ".tpl\" }}\n"
 
 #####
 ## Step 1 - Sync prometheus alert rules, create template file
@@ -23,15 +23,15 @@ charts = [
 'destination': 'helm/exporter-kube-state/', 'job_replace_by': '{{ template \"exporter-kube-state.fullname\" .  }}'},
 {'source':'contrib/kube-prometheus/assets/prometheus/rules/node.rules.yaml',
 'destination': 'helm/exporter-node/', 'job_replace_by': '{{ template \"exporter-node.fullname\" .  }}'},
-{'source':'contrib/kube-prometheus/assets/prometheus/rules/prometheus.rules.yaml', 
+{'source':'contrib/kube-prometheus/assets/prometheus/rules/prometheus.rules.yaml',
 'destination': 'helm/prometheus/', 'job_replace_by': '{{ template \"prometheus.fullname\" .  }}'},
-{'source':'contrib/kube-prometheus/assets/prometheus/rules/etcd3.rules.yaml', 
+{'source':'contrib/kube-prometheus/assets/prometheus/rules/etcd3.rules.yaml',
 'destination': 'helm/exporter-kube-etcd/', 'job_replace_by': '{{ template \"exporter-kube-etcd.fullname\" .  }}'},
-{'source':'contrib/kube-prometheus/assets/prometheus/rules/general.rules.yaml', 
+{'source':'contrib/kube-prometheus/assets/prometheus/rules/general.rules.yaml',
 'destination': 'helm/kube-prometheus/', 'job_replace_by': '{{ template \"kube-prometheus.fullname\" .  }}'},
-{'source':'contrib/kube-prometheus/assets/prometheus/rules/kubelet.rules.yaml', 
+{'source':'contrib/kube-prometheus/assets/prometheus/rules/kubelet.rules.yaml',
 'destination': 'helm/exporter-kubelets/', 'job_replace_by': 'kubelet'},
-{'source':'contrib/kube-prometheus/assets/prometheus/rules/kubernetes.rules.yaml', 
+{'source':'contrib/kube-prometheus/assets/prometheus/rules/kubernetes.rules.yaml',
 'destination': 'helm/exporter-kubernetes/', 'job_replace_by': 'kubernetes'},
 ]
 
@@ -44,30 +44,13 @@ for chart in charts:
   f = open(chart['source'], 'r')
   lines += escape(f.read())
   lines = re.sub("job=\"(.*?)\"", "job=\"" + chart['job_replace_by'] + "\"", lines) #replace the job name by chart variable
- 
+
   lines += "{{ end }}" # footer
-  
+
   new_f = "{}/templates/{}".format(chart['destination'], name)
 
-  # recreate the file  
+  # recreate the file
   with open(new_f, 'w') as f:
       f.write(lines)
 
-  print "Generated {}".format(new_f)
-
-######
-## Step 2 - Parse grafana dashboards, create a template file
-######
-
-with open('contrib/kube-prometheus/manifests/grafana/grafana-dashboards.yaml', 'r') as s:
-  data = yaml.load(s, Loader=yaml.RoundTripLoader)['data']
-
-# prometheus datasource it's not required now 
-del data['prometheus-datasource.json']
-
-data_s = get_header("grafana-dashboards.yaml")
-data_s += escape(yaml.dump(data, Dumper=yaml.RoundTripDumper))
-data_s += "{{ end }}" # footer
-
-with open('helm/grafana/templates/grafana-dashboards.yaml', 'w') as f:
-  f.write(data_s)
+  print("Generated {}".format(new_f))
