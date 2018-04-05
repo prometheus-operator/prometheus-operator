e9b12ee46e6402b4bfa31178da726965f30da8f8
Matthias Loibl <mail@matthiasloibl.com>
2018-04-05 20:53:35 +0200

    Remove LabelSelector from ServiceMonitor with StaticTargets

--- pkg/prometheus/promcfg_test.go
+++ pkg/prometheus/promcfg_test.go
@@ -321,25 +321,18 @@ func makeServiceMonitors() map[string]*monitoringv1.ServiceMonitor {
 			},
 		},
 		Spec: monitoringv1.ServiceMonitorSpec{
-			Selector: metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"group":  "group4",
-					"group3": "group5",
-				},
-			},
-			Endpoints: []monitoringv1.Endpoint{{
-				Port:     "web",
-				Interval: "30s",
-				Path:     "/federate",
-				Params:   map[string][]string{"metrics[]": {"{__name__=~\"job:.*\"}"}},
-				StaticTargets: &monitoringv1.StaticConfig{
-					Targets: []string{
+			Endpoints: []monitoringv1.Endpoint{
+				{
+					Port:     "web",
+					Interval: "30s",
+					Path:     "/federate",
+					Params:   map[string][]string{"metrics[]": {"{__name__=~\"job:.*\"}"}},
+					StaticTargets: []string{
 						"source-prometheus-1:9090",
 						"source-prometheus-2:9090",
 						"source-prometheus-3:9090",
 					},
 				},
-			},
 			},
 		},
 	}
