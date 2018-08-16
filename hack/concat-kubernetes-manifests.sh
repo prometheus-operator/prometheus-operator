652300d94790b759350a59d9c7f8b5c622765bbf
Max Leonard Inden <IndenML@gmail.com>
2018-08-16 11:07:36 +0200

    *.sh: Set sane bash options on shell scripts
    Adding the following accross the project:
    
    ```bash
    /# exit immediately when a command fails
    set -e
    /# only exit with zero if all commands of the pipeline exit successfully
    set -o pipefail
    /# error on unset variables
    +set -u
    
    ```

--- hack/concat-kubernetes-manifests.sh
+++ hack/concat-kubernetes-manifests.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# exit immediately when a command fails
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+# error on unset variables
+set -u
 
 # Concatenate all files with "---" because that's how to specify multiple
 # Kubernetes manifests in one file. Because the first `awk` also adds "---" in
--- scripts/check_license.sh
+++ scripts/check_license.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# exit immediately when a command fails
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+# error on unset variables
+set -u
 
 licRes=$(
     find . -type f -iname '*.go' ! -path '*/vendor/*' -exec \
