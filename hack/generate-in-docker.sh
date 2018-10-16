d2391e7ebefab80f44ce137b4f2846b3c111ce38
Frederic Branczyk <fbranczyk@gmail.com>
2018-10-16 15:16:35 +0200

    hack: Fix generate in docker script to actually run the command.

--- hack/generate-in-docker.sh
+++ hack/generate-in-docker.sh
@@ -5,6 +5,8 @@ set -e
 set -o pipefail
 # error on unset variables
 set -u
+# print commands
+set -x
 
 MFLAGS=${*:-} # MFLAGS are the parent make call's flags (see Makefile)
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
@@ -15,8 +17,8 @@ if hash getenforce 2> /dev/null && getenforce | grep 'Enforcing' > /dev/null; th
   VOLUME_OPTIONS=":Z"
 fi
 
-echo docker run \
+docker run \
     --rm \
     -u="$(id -u "$USER")":"$(id -g "$USER")" \
     -v "${SCRIPTDIR}/..:/go/src/github.com/coreos/prometheus-operator${VOLUME_OPTIONS}" \
-    po-jsonnet make "${MFLAGS}" generate
+    po-jsonnet make "${MFLAGS[@]}" generate
