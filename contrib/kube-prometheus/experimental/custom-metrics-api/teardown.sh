5a2ecd4aea776f2fedf6ba6b6b297965337fda87
Matthias Loibl <mail@matthiasloibl.com>
2018-11-13 14:43:53 +0100

    contrib/kube-prometheus: Add sample-app for showing custom metrics with HPA
    Use interval in ServiceMonitor endpoint
    
    Reintroduce updated dpeloy.sh & teardown.sh

---
+++ contrib/kube-prometheus/experimental/custom-metrics-api/teardown.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash	
+
+kubectl delete -n monitoring custom-metrics-apiserver-resource-reader-cluster-role-binding.yaml
+kubectl delete -n monitoring custom-metrics-apiservice.yaml
+kubectl delete -n monitoring custom-metrics-cluster-role.yaml
+kubectl delete -n monitoring custom-metrics-configmap.yaml
+kubectl delete -n monitoring hpa-custom-metrics-cluster-role-binding.yaml
