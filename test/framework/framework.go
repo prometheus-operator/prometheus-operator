b91d61b3f36272503c3c6969bf770323533e1295
Arno Uhlig <arno.uhlig@sap.com>
2019-05-21 12:32:29 +0200

    fix rbac for podmonitors

--- test/framework/framework.go
+++ test/framework/framework.go
@@ -202,6 +202,13 @@ func (f *Framework) CreatePrometheusOperator(ns, opImage string, namespacesToWat
 		return nil, errors.Wrap(err, "ServiceMonitor CRD not ready: %v\n")
 	}
 
+	err = k8sutil.WaitForCRDReady(func(opts metav1.ListOptions) (object runtime.Object, e error) {
+		return  f.MonClientV1.PodMonitors(v1.NamespaceAll).List(opts)
+	})
+	if err != nil {
+		return errors.Wrap(err, "PodMonitor CRD not ready: %v\n")
+	}
+
 	err = k8sutil.WaitForCRDReady(func(opts metav1.ListOptions) (runtime.Object, error) {
 		return f.MonClientV1.PrometheusRules(v1.NamespaceAll).List(opts)
 	})
