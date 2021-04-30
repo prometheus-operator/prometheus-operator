4169cd13decae1908ae07a0ea0a9afd892a840cb
paulfantom <pawel@krupa.net.pl>
2021-04-30 12:40:13 +0200

    pkg: readd app label to pods

--- pkg/alertmanager/statefulset.go
+++ pkg/alertmanager/statefulset.go
@@ -314,6 +314,8 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config) (*appsv1.S
 	podAnnotations := map[string]string{}
 	podLabels := map[string]string{}
 	podSelectorLabels := map[string]string{
+		// TODO(paulfantom): remove `app` label after 0.50 release
+		"app":                          "alertmanager",
 		"app.kubernetes.io/name":       "alertmanager",
 		"app.kubernetes.io/version":    amVersion,
 		"app.kubernetes.io/managed-by": "prometheus-operator",
