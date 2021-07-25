cb9805382b23bc045726ca58e86eb55c1340531b
Nicolas Takashi <nicolas.tcs@hotmail.com>
2021-07-25 14:12:47 +0100

    [FEAT] rollback ReadyTimeout  property

--- pkg/apis/monitoring/v1/types.go
+++ pkg/apis/monitoring/v1/types.go
@@ -640,6 +640,8 @@ type ThanosSpec struct {
 	LogFormat string `json:"logFormat,omitempty"`
 	// MinTime for Thanos sidecar to be configured with. Option can be a constant time in RFC3339 format or time duration relative to current time, such as -1d or 2h45m. Valid duration units are ms, s, m, h, d, w, y.
 	MinTime string `json:"minTime,omitempty"`
+	// ReadyTimeout is the maximum time Thanos sidecar will wait for Prometheus to start. Eg 10m
+	ReadyTimeout string `json:"readyTimeout,omitempty"`
 	// VolumeMounts allows configuration of additional VolumeMounts on the output StatefulSet definition.
 	// VolumeMounts specified will be appended to other VolumeMounts in the thanos-sidecar container.
 	VolumeMounts []v1.VolumeMount `json:"volumeMounts,omitempty"`
