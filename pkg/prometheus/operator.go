e92395600dea757e6f031a69d2d3b698ceb45441
gitgaoxiang <gao_xiang@126.com>
2022-08-29 11:20:56 +0800

    replace the List with a set of Get's

--- pkg/prometheus/operator.go
+++ pkg/prometheus/operator.go
@@ -1960,27 +1960,17 @@ func (c *Operator) createOrUpdateConfigurationSecret(ctx context.Context, p *mon
 	sClient := c.kclient.CoreV1().Secrets(p.Namespace)	
 	var SecretsInPromNS *v1.SecretList
 	if p.Spec.AdditionalScrapeConfigs != nil && p.Spec.AdditionalScrapeConfigs.Name != "" {
-		ScrapeConfigSecret, err := sClient.Get(ctx, p.Spec.AdditionalScrapeConfigs.Name, metav1.GetOptions{})
-		if err != nil {
-			return err
-		}
+		ScrapeConfigSecret, _ := sClient.Get(ctx, p.Spec.AdditionalScrapeConfigs.Name, metav1.GetOptions{})
 		SecretsInPromNS.Items = append(SecretsInPromNS.Items, *ScrapeConfigSecret)
 	}
 	if p.Spec.AdditionalAlertRelabelConfigs != nil && p.Spec.AdditionalAlertRelabelConfigs.Name != "" {
-		RelabelConfigSecret, err := sClient.Get(ctx, p.Spec.AdditionalAlertRelabelConfigs.Name, metav1.GetOptions{})
-		if err != nil {
-			return err
-		}
+		RelabelConfigSecret, _ := sClient.Get(ctx, p.Spec.AdditionalAlertRelabelConfigs.Name, metav1.GetOptions{})
 		SecretsInPromNS.Items = append(SecretsInPromNS.Items, *RelabelConfigSecret)
 	}
 	if p.Spec.AdditionalAlertManagerConfigs != nil && p.Spec.AdditionalAlertManagerConfigs.Name != "" {
-		ManagerConfigSecret, err := sClient.Get(ctx, p.Spec.AdditionalAlertManagerConfigs.Name, metav1.GetOptions{})
-		if err != nil {
-			return err
-		}
+		ManagerConfigSecret, _ := sClient.Get(ctx, p.Spec.AdditionalAlertManagerConfigs.Name, metav1.GetOptions{})
 		SecretsInPromNS.Items = append(SecretsInPromNS.Items, *ManagerConfigSecret)
-	}	
-
+	}
 	for i, remote := range p.Spec.RemoteRead {
 		if err := store.AddBasicAuth(ctx, p.GetNamespace(), remote.BasicAuth, fmt.Sprintf("remoteRead/%d", i)); err != nil {
 			return errors.Wrapf(err, "remote read %d", i)
