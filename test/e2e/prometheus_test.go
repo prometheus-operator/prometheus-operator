prometheus-operator/prometheus-operator

commit 84ffc50f68f619c0cb2e3e6999ea267a259b4635

fix: invalid pod definition due to long volume names (#4988)
Previously, adding secrets to Prometheus resource longer than 50
characters resulted in generation of statefulset that have
`volume.name` larger than 63 characters. This doesn't conform to
RFC-1123 (<= 63 chars) requirement and resulted in pod not getting
created.

This patch fixes the issue by ensuring that the volume name conform to
the requirement. Additionally it fixes an issue with SanitizeVolumeName
which used to truncate the name at DNS1123LabelMaxLength thus returing
same name for `<63-char>-foo` and `<63-char>-bar` to return unique name
by computing the sha1 and appending to the end.

This commit also ensures that secrets and configmaps provided via the
Alertmanager and Prometheus have correct volume mounts.

Bug: https://github.com/prometheus-operator/prometheus-operator/issues/4944

Signed-off-by: Simon Pasquier <spasquie@redhat.com>
Signed-off-by: Sunil Thaha <sthaha@redhat.com>

Signed-off-by: Simon Pasquier <spasquie@redhat.com>
Signed-off-by: Sunil Thaha <sthaha@redhat.com>

pkg/alertmanager/statefulset.go
@@ -512,9 +512,15 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 		},
 	}
 
+	rn := k8sutil.NewResourceNamerWithPrefix("secret")
 	for _, s := range a.Spec.Secrets {
+		name, err := rn.UniqueVolumeName(s)
+		if err != nil {
+			return nil, err
+		}
+
 		volumes = append(volumes, v1.Volume{
-			Name: k8sutil.SanitizeVolumeName("secret-" + s),
+			Name: name,
 			VolumeSource: v1.VolumeSource{
 				Secret: &v1.SecretVolumeSource{
 					SecretName: s,
@@ -523,7 +529,7 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 		})
 		mountPath := secretsDir + s
 		mount := v1.VolumeMount{
-			Name:      k8sutil.SanitizeVolumeName("secret-" + s),
+			Name:      name,
 			ReadOnly:  true,
 			MountPath: mountPath,
 		}
@@ -532,9 +538,15 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 		watchedDirectories = append(watchedDirectories, mountPath)
 	}
 
+	rn = k8sutil.NewResourceNamerWithPrefix("configmap")
 	for _, c := range a.Spec.ConfigMaps {
+		name, err := rn.UniqueVolumeName(c)
+		if err != nil {
+			return nil, err
+		}
+
 		volumes = append(volumes, v1.Volume{
-			Name: k8sutil.SanitizeVolumeName("configmap-" + c),
+			Name: name,
 			VolumeSource: v1.VolumeSource{
 				ConfigMap: &v1.ConfigMapVolumeSource{
 					LocalObjectReference: v1.LocalObjectReference{
@@ -545,7 +557,7 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 		})
 		mountPath := configmapsDir + c
 		mount := v1.VolumeMount{
-			Name:      k8sutil.SanitizeVolumeName("configmap-" + c),
+			Name:      name,
 			ReadOnly:  true,
 			MountPath: mountPath,
 		}
@@ -571,7 +583,10 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 			return nil, err
 		}
 
-		confArg, configVol, configMount := webConfig.GetMountParameters()
+		confArg, configVol, configMount, err := webConfig.GetMountParameters()
+		if err != nil {
+			return nil, err
+		}
 		amArgs = append(amArgs, fmt.Sprintf("--%s=%s", confArg.Name, confArg.Value))
 		volumes = append(volumes, configVol...)
 		amVolumeMounts = append(amVolumeMounts, configMount...)

pkg/k8sutil/k8sutil.go
@@ -16,6 +16,7 @@ package k8sutil
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -23,6 +24,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/cespare/xxhash/v2"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	monitoringv1alpha1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1alpha1"
 	monitoringv1beta1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1beta1"
@@ -232,15 +234,57 @@ func IsAPIGroupVersionResourceSupported(discoveryCli discovery.DiscoveryInterfac
 	return false, nil
 }
 
-// SanitizeVolumeName ensures that the given volume name is a valid DNS-1123 label
-// accepted by Kubernetes.
-func SanitizeVolumeName(name string) string {
+// ResourceNamer knows how to generate valid names for various Kubernetes resources.
+type ResourceNamer struct {
+	prefix string
+}
+
+// NewResourceNamerWithPrefix returns a ResourceNamer that adds a prefix
+// followed by an hyphen character to all resource names.
+func NewResourceNamerWithPrefix(p string) ResourceNamer {
+	return ResourceNamer{prefix: p}
+}
+
+// UniqueVolumeName returns a volume name that is a valid DNS-1123 label.
+// The returned name has a hash-based suffix to ensure uniqueness in case the
+// input name exceeds the 63-chars limit.
+func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
+	if rn.prefix != "" {
+		name = strings.TrimRight(rn.prefix, "-") + "-" + name
+	}
+
 	name = strings.ToLower(name)
 	name = invalidDNS1123Characters.ReplaceAllString(name, "-")
-	if len(name) > validation.DNS1123LabelMaxLength {
-		name = name[0:validation.DNS1123LabelMaxLength]
+	name = strings.Trim(name, "-")
+
+	// Hash the name and append the 8 first characters of the hash
+	// value to the resulting name to ensure that 2 names longer than
+	// DNS1123LabelMaxLength return unique volume names.
+	// E.g. long-63-chars-abc, long-63-chars-XYZ may be added to
+	// volume name since they are trimmed at long-63-chars, there will be 2
+	// volume entries with the same name.
+	// In practice, the hash is computed for the full name then trimmed to
+	// the first 8 chars and added to the end:
+	// * long-63-chars-abc -> first-54-chars-deadbeef
+	// * long-63-chars-XYZ -> first-54-chars-d3adb33f
+	xxh := xxhash.New()
+	if _, err := xxh.Write([]byte(name)); err != nil {
+		return "", err
 	}
-	return strings.Trim(name, "-")
+
+	h := fmt.Sprintf("-%x", xxh.Sum64())
+	h = h[:9]
+
+	if len(name) > validation.DNS1123LabelMaxLength-9 {
+		name = name[:validation.DNS1123LabelMaxLength-9]
+	}
+
+	name = name + h
+	if errs := validation.IsDNS1123Label(name); len(errs) > 0 {
+		return "", errors.New(strings.Join(errs, ","))
+	}
+
+	return name, nil
 }
 
 // AddTypeInformationToObject adds TypeMeta information to a runtime.Object based upon the loaded scheme.Scheme

pkg/prometheus/statefulset.go
@@ -569,7 +569,10 @@ func makeStatefulSetSpec(
 			return nil, err
 		}
 
-		confArg, configVol, configMount := webConfig.GetMountParameters()
+		confArg, configVol, configMount, err := webConfig.GetMountParameters()
+		if err != nil {
+			return nil, err
+		}
 		promArgs = append(promArgs, confArg)
 		volumes = append(volumes, configVol...)
 		promVolumeMounts = append(promVolumeMounts, configMount...)
@@ -577,9 +580,15 @@ func makeStatefulSetSpec(
 
 	// Mount related secrets
 
+	rn := k8sutil.NewResourceNamerWithPrefix("secret")
 	for _, s := range p.Spec.Secrets {
+		name, err := rn.UniqueVolumeName(s)
+		if err != nil {
+			return nil, err
+		}
+
 		volumes = append(volumes, v1.Volume{
-			Name: k8sutil.SanitizeVolumeName("secret-" + s),
+			Name: name,
 			VolumeSource: v1.VolumeSource{
 				Secret: &v1.SecretVolumeSource{
 					SecretName: s,
@@ -587,15 +596,21 @@ func makeStatefulSetSpec(
 			},
 		})
 		promVolumeMounts = append(promVolumeMounts, v1.VolumeMount{
-			Name:      k8sutil.SanitizeVolumeName("secret-" + s),
+			Name:      name,
 			ReadOnly:  true,
 			MountPath: secretsDir + s,
 		})
 	}
 
+	rn = k8sutil.NewResourceNamerWithPrefix("configmap")
 	for _, c := range p.Spec.ConfigMaps {
+		name, err := rn.UniqueVolumeName(c)
+		if err != nil {
+			return nil, err
+		}
+
 		volumes = append(volumes, v1.Volume{
-			Name: k8sutil.SanitizeVolumeName("configmap-" + c),
+			Name: name,
 			VolumeSource: v1.VolumeSource{
 				ConfigMap: &v1.ConfigMapVolumeSource{
 					LocalObjectReference: v1.LocalObjectReference{
@@ -605,7 +620,7 @@ func makeStatefulSetSpec(
 			},
 		})
 		promVolumeMounts = append(promVolumeMounts, v1.VolumeMount{
-			Name:      k8sutil.SanitizeVolumeName("configmap-" + c),
+			Name:      name,
 			ReadOnly:  true,
 			MountPath: configmapsDir + c,
 		})

pkg/alertmanager/statefulset_test.go
@@ -569,10 +569,10 @@ func TestAdditionalSecretsMounted(t *testing.T) {
 	secret1Found = false
 	secret2Found = false
 	for _, v := range sset.Spec.Template.Spec.Containers[0].VolumeMounts {
-		if v.Name == "secret-secret1" && v.MountPath == "/etc/alertmanager/secrets/secret1" {
+		if strings.HasPrefix(v.Name, "secret-secret1-") && v.MountPath == "/etc/alertmanager/secrets/secret1" {
 			secret1Found = true
 		}
-		if v.Name == "secret-secret2" && v.MountPath == "/etc/alertmanager/secrets/secret2" {
+		if strings.HasPrefix(v.Name, "secret-secret2-") && v.MountPath == "/etc/alertmanager/secrets/secret2" {
 			secret2Found = true
 		}
 	}
@@ -722,8 +722,9 @@ func TestAdditionalConfigMap(t *testing.T) {
 
 	cmVolumeFound := false
 	for _, v := range sset.Spec.Template.Spec.Volumes {
-		if v.Name == "configmap-test-cm1" {
+		if strings.HasPrefix(v.Name, "configmap-test-cm1-") {
 			cmVolumeFound = true
+			break
 		}
 	}
 	if !cmVolumeFound {
@@ -732,8 +733,9 @@ func TestAdditionalConfigMap(t *testing.T) {
 
 	cmMounted := false
 	for _, v := range sset.Spec.Template.Spec.Containers[0].VolumeMounts {
-		if v.Name == "configmap-test-cm1" && v.MountPath == "/etc/alertmanager/configmaps/test-cm1" {
+		if strings.HasPrefix(v.Name, "configmap-test-cm1-") && v.MountPath == "/etc/alertmanager/configmaps/test-cm1" {
 			cmMounted = true
+			break
 		}
 	}
 	if !cmMounted {

pkg/k8sutil/k8sutil_test.go
@@ -29,42 +29,100 @@ import (
 	"k8s.io/apimachinery/pkg/util/validation"
 )
 
-func Test_SanitizeVolumeName(t *testing.T) {
+func TestUniqueVolumeName(t *testing.T) {
 	cases := []struct {
+		prefix   string
 		name     string
 		expected string
+		err      bool
 	}{
 		{
 			name:     "@$!!@$%!#$%!#$%!#$!#$%%$#@!#",
 			expected: "",
+			err:      true,
 		},
 		{
 			name:     "NAME",
-			expected: "name",
+			expected: "name-6c5f7b2e",
 		},
 		{
 			name:     "foo--",
-			expected: "foo",
+			expected: "foo-33bf00a8",
 		},
 		{
 			name:     "foo^%#$bar",
-			expected: "foo-bar",
+			expected: "foo-bar-6ab521af",
 		},
 		{
 			name:     "fOo^%#$bar",
-			expected: "foo-bar",
+			expected: "foo-bar-6ab521af",
 		},
 		{
-			name:     strings.Repeat("a", validation.DNS1123LabelMaxLength*2),
-			expected: strings.Repeat("a", validation.DNS1123LabelMaxLength),
+			name: strings.Repeat("a", validation.DNS1123LabelMaxLength*2),
+			expected: strings.Repeat("a", validation.DNS1123LabelMaxLength-9) +
+				"-4ed69ce2",
+		},
+		{
+			prefix:   "with-prefix",
+			name:     "name",
+			expected: "with-prefix-name-fa429965",
+		},
+		{
+			prefix:   "with-prefix-",
+			name:     "name",
+			expected: "with-prefix-name-fa429965",
+		},
+		{
+			prefix: "with-prefix",
+			name:   strings.Repeat("a", validation.DNS1123LabelMaxLength*2),
+			expected: "with-prefix-" + strings.Repeat("a", 42) +
+				"-c183166e",
 		},
 	}
 
 	for i, c := range cases {
-		out := SanitizeVolumeName(c.name)
-		if c.expected != out {
-			t.Errorf("expected test case %d to be %q but got %q", i, c.expected, out)
-		}
+		t.Run(c.name, func(t *testing.T) {
+			rn := ResourceNamer{prefix: c.prefix}
+
+			out, err := rn.UniqueVolumeName(c.name)
+			if c.err {
+				if err == nil {
+					t.Errorf("expecting error, got nil")
+				}
+				return
+			}
+
+			if err != nil {
+				t.Errorf("expecting no error, got %v", err)
+			}
+
+			if c.expected != out {
+				t.Errorf("expected test case %d to be %q but got %q", i, c.expected, out)
+			}
+		})
+	}
+}
+
+func TestUniqueVolumeNameCollision(t *testing.T) {
+	// a<63>-foo
+	foo := strings.Repeat("a", validation.DNS1123LabelMaxLength) + "foo"
+	// a<63>-bar
+	bar := strings.Repeat("a", validation.DNS1123LabelMaxLength) + "bar"
+
+	rn := ResourceNamer{}
+
+	fooSanitized, err := rn.UniqueVolumeName(foo)
+	if err != nil {
+		t.Errorf("expecting no error, got %v", err)
+	}
+
+	barSanitized, err := rn.UniqueVolumeName(bar)
+	if err != nil {
+		t.Errorf("expecting no error, got %v", err)
+	}
+
+	if fooSanitized == barSanitized {
+		t.Fatalf("expected sanitized volume name of %q and %q to be different but got %q", foo, bar, fooSanitized)
 	}
 }
 

pkg/prometheus/statefulset_test.go
@@ -316,7 +316,7 @@ func TestStatefulSetVolumeInitial(t *testing.T) {
 									SubPath:   "web-config.yaml",
 								},
 								{
-									Name:      "secret-test-secret1",
+									Name:      "secret-test-secret1-df14634a",
 									ReadOnly:  true,
 									MountPath: "/etc/prometheus/secrets/test-secret1",
 									SubPath:   "",
@@ -374,7 +374,7 @@ func TestStatefulSetVolumeInitial(t *testing.T) {
 							},
 						},
 						{
-							Name: "secret-test-secret1",
+							Name: "secret-test-secret1-df14634a",
 							VolumeSource: v1.VolumeSource{
 								Secret: &v1.SecretVolumeSource{
 									SecretName: "test-secret1",
@@ -433,7 +433,7 @@ func TestAdditionalConfigMap(t *testing.T) {
 
 	cmVolumeFound := false
 	for _, v := range sset.Spec.Template.Spec.Volumes {
-		if v.Name == "configmap-test-cm1" {
+		if strings.HasPrefix(v.Name, "configmap-test-cm1") {
 			cmVolumeFound = true
 		}
 	}
@@ -443,7 +443,7 @@ func TestAdditionalConfigMap(t *testing.T) {
 
 	cmMounted := false
 	for _, v := range sset.Spec.Template.Spec.Containers[0].VolumeMounts {
-		if v.Name == "configmap-test-cm1" && v.MountPath == "/etc/prometheus/configmaps/test-cm1" {
+		if strings.HasPrefix(v.Name, "configmap-test-cm1") && v.MountPath == "/etc/prometheus/configmaps/test-cm1" {
 			cmMounted = true
 		}
 	}

pkg/webconfig/config_test.go
@@ -19,6 +19,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/kylelemons/godebug/pretty"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/prometheus-operator/prometheus-operator/pkg/webconfig"
 	v1 "k8s.io/api/core/v1"
@@ -292,7 +293,7 @@ func TestGetMountParameters(t *testing.T) {
 					},
 				},
 				{
-					Name: "web-config-tls-secret-key-some-secret",
+					Name: "web-config-tls-secret-key-some-secret-38b2f493",
 					VolumeSource: v1.VolumeSource{
 						Secret: &v1.SecretVolumeSource{
 							SecretName: "some-secret",
@@ -300,7 +301,7 @@ func TestGetMountParameters(t *testing.T) {
 					},
 				},
 				{
-					Name: "web-config-tls-secret-cert-some-secret",
+					Name: "web-config-tls-secret-cert-some-secret-a60e0a56",
 					VolumeSource: v1.VolumeSource{
 						Secret: &v1.SecretVolumeSource{
 							SecretName: "some-secret",
@@ -308,7 +309,7 @@ func TestGetMountParameters(t *testing.T) {
 					},
 				},
 				{
-					Name: "web-config-tls-secret-client-ca-some-secret",
+					Name: "web-config-tls-secret-client-ca-some-secret-3ec4509b",
 					VolumeSource: v1.VolumeSource{
 						Secret: &v1.SecretVolumeSource{
 							SecretName: "some-secret",
@@ -326,7 +327,7 @@ func TestGetMountParameters(t *testing.T) {
 					SubPathExpr:      "",
 				},
 				{
-					Name:             "web-config-tls-secret-key-some-secret",
+					Name:             "web-config-tls-secret-key-some-secret-38b2f493",
 					ReadOnly:         true,
 					MountPath:        "/etc/prometheus/web_config/secret_some-secret_tls.key",
 					SubPath:          "tls.key",
@@ -334,7 +335,7 @@ func TestGetMountParameters(t *testing.T) {
 					SubPathExpr:      "",
 				},
 				{
-					Name:             "web-config-tls-secret-cert-some-secret",
+					Name:             "web-config-tls-secret-cert-some-secret-a60e0a56",
 					ReadOnly:         true,
 					MountPath:        "/etc/prometheus/web_config/secret_some-secret_tls.crt",
 					SubPath:          "tls.crt",
@@ -342,7 +343,7 @@ func TestGetMountParameters(t *testing.T) {
 					SubPathExpr:      "",
 				},
 				{
-					Name:             "web-config-tls-secret-client-ca-some-secret",
+					Name:             "web-config-tls-secret-client-ca-some-secret-3ec4509b",
 					ReadOnly:         true,
 					MountPath:        "/etc/prometheus/web_config/secret_some-secret_tls.client_ca",
 					SubPath:          "tls.client_ca",
@@ -354,19 +355,27 @@ func TestGetMountParameters(t *testing.T) {
 	}
 
 	for _, tt := range ts {
-		tlsAssets, err := webconfig.New("/etc/prometheus/web_config", "web-config", tt.webConfigFileFields)
-		if err != nil {
-			t.Fatal(err)
-		}
+		t.Run("", func(t *testing.T) {
+			tlsAssets, err := webconfig.New("/etc/prometheus/web_config", "web-config", tt.webConfigFileFields)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-		_, volumes, mounts := tlsAssets.GetMountParameters()
+			_, volumes, mounts, err := tlsAssets.GetMountParameters()
 
-		if !reflect.DeepEqual(volumes, tt.expectedVolumes) {
-			t.Errorf("invalid volumes,\ngot  %v,\nwant %v", volumes, tt.expectedVolumes)
-		}
+			if err != nil {
+				t.Fatalf("expecting no error, got %v", err)
+			}
 
-		if !reflect.DeepEqual(mounts, tt.expectedMounts) {
-			t.Errorf("invalid mounts,\ngot  %v,\nwant %v", mounts, tt.expectedMounts)
-		}
+			if !reflect.DeepEqual(volumes, tt.expectedVolumes) {
+				t.Log(pretty.Compare(tt.expectedVolumes, volumes))
+				t.Errorf("invalid volumes")
+			}
+
+			if !reflect.DeepEqual(mounts, tt.expectedMounts) {
+				t.Log(pretty.Compare(tt.expectedMounts, mounts))
+				t.Errorf("invalid mounts")
+			}
+		})
 	}
 }

commit 3b53da78df4961c41b85496dca5671579b80d83d

Fixed secret and configmap volume names that need to be mounted in additional containers (#5000)
* test/e2e: add testPromStrategicMergePatch

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

* *: fix volume names for mounted secrets/configmaps

This change fixes a regression introduced in #4988 (84ffc50).

The volume names for additional secrets/configmaps provided by
`.spec.secrets` and `.spec.configMaps` shouldn't be suffixed by the hash
ID as it is done for the webconfig secrets/configmaps. Otherwise it
is impossible to mount them in a sidecar container since there's no way
to know the volume name in advance.

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

pkg/alertmanager/statefulset.go
@@ -514,7 +514,7 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 
 	rn := k8sutil.NewResourceNamerWithPrefix("secret")
 	for _, s := range a.Spec.Secrets {
-		name, err := rn.UniqueVolumeName(s)
+		name, err := rn.VolumeName(s)
 		if err != nil {
 			return nil, err
 		}
@@ -540,7 +540,7 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 
 	rn = k8sutil.NewResourceNamerWithPrefix("configmap")
 	for _, c := range a.Spec.ConfigMaps {
-		name, err := rn.UniqueVolumeName(c)
+		name, err := rn.VolumeName(c)
 		if err != nil {
 			return nil, err
 		}

pkg/k8sutil/k8sutil.go
@@ -245,10 +245,7 @@ func NewResourceNamerWithPrefix(p string) ResourceNamer {
 	return ResourceNamer{prefix: p}
 }
 
-// UniqueVolumeName returns a volume name that is a valid DNS-1123 label.
-// The returned name has a hash-based suffix to ensure uniqueness in case the
-// input name exceeds the 63-chars limit.
-func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
+func (rn ResourceNamer) sanitizedVolumeName(name string) string {
 	if rn.prefix != "" {
 		name = strings.TrimRight(rn.prefix, "-") + "-" + name
 	}
@@ -257,6 +254,21 @@ func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
 	name = invalidDNS1123Characters.ReplaceAllString(name, "-")
 	name = strings.Trim(name, "-")
 
+	return name
+}
+
+func isValidDNS1123Label(name string) error {
+	if errs := validation.IsDNS1123Label(name); len(errs) > 0 {
+		return errors.New(strings.Join(errs, ","))
+	}
+
+	return nil
+}
+
+// UniqueVolumeName returns a volume name that is a valid DNS-1123 label.
+// The returned name has a hash-based suffix to ensure uniqueness in case the
+// input name exceeds the 63-chars limit.
+func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
 	// Hash the name and append the 8 first characters of the hash
 	// value to the resulting name to ensure that 2 names longer than
 	// DNS1123LabelMaxLength return unique volume names.
@@ -275,6 +287,8 @@ func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
 	h := fmt.Sprintf("-%x", xxh.Sum64())
 	h = h[:9]
 
+	name = rn.sanitizedVolumeName(name)
+
 	if len(name) > validation.DNS1123LabelMaxLength-9 {
 		name = name[:validation.DNS1123LabelMaxLength-9]
 	}
@@ -284,7 +298,18 @@ func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
 		return "", errors.New(strings.Join(errs, ","))
 	}
 
-	return name, nil
+	return name, isValidDNS1123Label(name)
+}
+
+// VolumeName returns a volume name that is a valid DNS-1123 label.
+func (rn ResourceNamer) VolumeName(name string) (string, error) {
+	name = rn.sanitizedVolumeName(name)
+
+	if len(name) > validation.DNS1123LabelMaxLength {
+		name = name[:validation.DNS1123LabelMaxLength]
+	}
+
+	return name, isValidDNS1123Label(name)
 }
 
 // AddTypeInformationToObject adds TypeMeta information to a runtime.Object based upon the loaded scheme.Scheme

pkg/prometheus/statefulset.go
@@ -582,7 +582,7 @@ func makeStatefulSetSpec(
 
 	rn := k8sutil.NewResourceNamerWithPrefix("secret")
 	for _, s := range p.Spec.Secrets {
-		name, err := rn.UniqueVolumeName(s)
+		name, err := rn.VolumeName(s)
 		if err != nil {
 			return nil, err
 		}
@@ -604,7 +604,7 @@ func makeStatefulSetSpec(
 
 	rn = k8sutil.NewResourceNamerWithPrefix("configmap")
 	for _, c := range p.Spec.ConfigMaps {
-		name, err := rn.UniqueVolumeName(c)
+		name, err := rn.VolumeName(c)
 		if err != nil {
 			return nil, err
 		}

pkg/alertmanager/statefulset_test.go
@@ -569,10 +569,10 @@ func TestAdditionalSecretsMounted(t *testing.T) {
 	secret1Found = false
 	secret2Found = false
 	for _, v := range sset.Spec.Template.Spec.Containers[0].VolumeMounts {
-		if strings.HasPrefix(v.Name, "secret-secret1-") && v.MountPath == "/etc/alertmanager/secrets/secret1" {
+		if v.Name == "secret-secret1" && v.MountPath == "/etc/alertmanager/secrets/secret1" {
 			secret1Found = true
 		}
-		if strings.HasPrefix(v.Name, "secret-secret2-") && v.MountPath == "/etc/alertmanager/secrets/secret2" {
+		if v.Name == "secret-secret2" && v.MountPath == "/etc/alertmanager/secrets/secret2" {
 			secret2Found = true
 		}
 	}
@@ -722,7 +722,7 @@ func TestAdditionalConfigMap(t *testing.T) {
 
 	cmVolumeFound := false
 	for _, v := range sset.Spec.Template.Spec.Volumes {
-		if strings.HasPrefix(v.Name, "configmap-test-cm1-") {
+		if v.Name == "configmap-test-cm1" {
 			cmVolumeFound = true
 			break
 		}
@@ -733,7 +733,7 @@ func TestAdditionalConfigMap(t *testing.T) {
 
 	cmMounted := false
 	for _, v := range sset.Spec.Template.Spec.Containers[0].VolumeMounts {
-		if strings.HasPrefix(v.Name, "configmap-test-cm1-") && v.MountPath == "/etc/alertmanager/configmaps/test-cm1" {
+		if v.Name == "configmap-test-cm1" && v.MountPath == "/etc/alertmanager/configmaps/test-cm1" {
 			cmMounted = true
 			break
 		}

pkg/k8sutil/k8sutil_test.go
@@ -43,19 +43,19 @@ func TestUniqueVolumeName(t *testing.T) {
 		},
 		{
 			name:     "NAME",
-			expected: "name-6c5f7b2e",
+			expected: "name-4cfd3574",
 		},
 		{
 			name:     "foo--",
-			expected: "foo-33bf00a8",
+			expected: "foo-e705c7c8",
 		},
 		{
 			name:     "foo^%#$bar",
-			expected: "foo-bar-6ab521af",
+			expected: "foo-bar-f3e212b1",
 		},
 		{
 			name:     "fOo^%#$bar",
-			expected: "foo-bar-6ab521af",
+			expected: "foo-bar-ee5c3c18",
 		},
 		{
 			name: strings.Repeat("a", validation.DNS1123LabelMaxLength*2),
@@ -65,18 +65,17 @@ func TestUniqueVolumeName(t *testing.T) {
 		{
 			prefix:   "with-prefix",
 			name:     "name",
-			expected: "with-prefix-name-fa429965",
+			expected: "with-prefix-name-6c5f7b2e",
 		},
 		{
 			prefix:   "with-prefix-",
 			name:     "name",
-			expected: "with-prefix-name-fa429965",
+			expected: "with-prefix-name-6c5f7b2e",
 		},
 		{
-			prefix: "with-prefix",
-			name:   strings.Repeat("a", validation.DNS1123LabelMaxLength*2),
-			expected: "with-prefix-" + strings.Repeat("a", 42) +
-				"-c183166e",
+			prefix:   "with-prefix",
+			name:     strings.Repeat("a", validation.DNS1123LabelMaxLength*2),
+			expected: "with-prefix-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-4ed69ce2",
 		},
 	}
 

pkg/prometheus/statefulset_test.go
@@ -316,7 +316,7 @@ func TestStatefulSetVolumeInitial(t *testing.T) {
 									SubPath:   "web-config.yaml",
 								},
 								{
-									Name:      "secret-test-secret1-df14634a",
+									Name:      "secret-test-secret1",
 									ReadOnly:  true,
 									MountPath: "/etc/prometheus/secrets/test-secret1",
 									SubPath:   "",
@@ -374,7 +374,7 @@ func TestStatefulSetVolumeInitial(t *testing.T) {
 							},
 						},
 						{
-							Name: "secret-test-secret1-df14634a",
+							Name: "secret-test-secret1",
 							VolumeSource: v1.VolumeSource{
 								Secret: &v1.SecretVolumeSource{
 									SecretName: "test-secret1",

pkg/webconfig/config_test.go
@@ -293,7 +293,7 @@ func TestGetMountParameters(t *testing.T) {
 					},
 				},
 				{
-					Name: "web-config-tls-secret-key-some-secret-38b2f493",
+					Name: "web-config-tls-secret-key-some-secret-3556f148",
 					VolumeSource: v1.VolumeSource{
 						Secret: &v1.SecretVolumeSource{
 							SecretName: "some-secret",
@@ -301,7 +301,7 @@ func TestGetMountParameters(t *testing.T) {
 					},
 				},
 				{
-					Name: "web-config-tls-secret-cert-some-secret-a60e0a56",
+					Name: "web-config-tls-secret-cert-some-secret-3556f148",
 					VolumeSource: v1.VolumeSource{
 						Secret: &v1.SecretVolumeSource{
 							SecretName: "some-secret",
@@ -309,7 +309,7 @@ func TestGetMountParameters(t *testing.T) {
 					},
 				},
 				{
-					Name: "web-config-tls-secret-client-ca-some-secret-3ec4509b",
+					Name: "web-config-tls-secret-client-ca-some-secret-3556f148",
 					VolumeSource: v1.VolumeSource{
 						Secret: &v1.SecretVolumeSource{
 							SecretName: "some-secret",
@@ -327,7 +327,7 @@ func TestGetMountParameters(t *testing.T) {
 					SubPathExpr:      "",
 				},
 				{
-					Name:             "web-config-tls-secret-key-some-secret-38b2f493",
+					Name:             "web-config-tls-secret-key-some-secret-3556f148",
 					ReadOnly:         true,
 					MountPath:        "/etc/prometheus/web_config/secret_some-secret_tls.key",
 					SubPath:          "tls.key",
@@ -335,7 +335,7 @@ func TestGetMountParameters(t *testing.T) {
 					SubPathExpr:      "",
 				},
 				{
-					Name:             "web-config-tls-secret-cert-some-secret-a60e0a56",
+					Name:             "web-config-tls-secret-cert-some-secret-3556f148",
 					ReadOnly:         true,
 					MountPath:        "/etc/prometheus/web_config/secret_some-secret_tls.crt",
 					SubPath:          "tls.crt",
@@ -343,7 +343,7 @@ func TestGetMountParameters(t *testing.T) {
 					SubPathExpr:      "",
 				},
 				{
-					Name:             "web-config-tls-secret-client-ca-some-secret-3ec4509b",
+					Name:             "web-config-tls-secret-client-ca-some-secret-3556f148",
 					ReadOnly:         true,
 					MountPath:        "/etc/prometheus/web_config/secret_some-secret_tls.client_ca",
 					SubPath:          "tls.client_ca",

test/e2e/main_test.go
@@ -222,6 +222,7 @@ func testAllNSPrometheus(t *testing.T) {
 		"PromNamespaceEnforcementExclusion":      testPromNamespaceEnforcementExclusion,
 		"PromQueryLogFile":                       testPromQueryLogFile,
 		"PromDegradedCondition":                  testPromDegradedConditionStatus,
+		"PromStrategicMergePatch":                testPromStrategicMergePatch,
 	}
 
 	for name, f := range testFuncs {

test/e2e/prometheus_test.go
@@ -4486,6 +4486,53 @@ func testPromDegradedConditionStatus(t *testing.T) {
 	}
 }
 
+func testPromStrategicMergePatch(t *testing.T) {
+	t.Parallel()
+	testCtx := framework.NewTestCtx(t)
+	defer testCtx.Cleanup(t)
+	ns := framework.CreateNamespace(context.Background(), t, testCtx)
+	framework.SetupPrometheusRBAC(context.Background(), t, testCtx, ns)
+
+	secret := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: "secret", Namespace: ns},
+		Type:       v1.SecretType("Opaque"),
+		Data:       map[string][]byte{},
+	}
+	_, err := framework.KubeClient.CoreV1().Secrets(ns).Create(context.Background(), secret, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("failed to create secret: %s", err)
+	}
+
+	configmap := &v1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{Name: "configmap", Namespace: ns},
+		Data:       map[string]string{},
+	}
+	_, err = framework.KubeClient.CoreV1().ConfigMaps(ns).Create(context.Background(), configmap, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("failed to create configmap: %s", err)
+	}
+
+	p := framework.MakeBasicPrometheus(ns, "test", "", 1)
+	p.Spec.Secrets = []string{secret.Name}
+	p.Spec.ConfigMaps = []string{configmap.Name}
+	p.Spec.Containers = []v1.Container{{
+		Name:  "sidecar",
+		Image: "nginx",
+		// Ensure that the sidecar container can mount the additional secret and configmap.
+		VolumeMounts: []v1.VolumeMount{{
+			Name:      "secret-" + secret.Name,
+			MountPath: "/tmp/secret",
+		}, {
+			Name:      "configmap-" + configmap.Name,
+			MountPath: "/tmp/configmap",
+		}},
+	}}
+
+	if _, err := framework.CreatePrometheusAndWaitUntilReady(context.Background(), ns, p); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func isAlertmanagerDiscoveryWorking(ctx context.Context, ns, promSVCName, alertmanagerName string) func() (bool, error) {
 	return func() (bool, error) {
 		pods, err := framework.KubeClient.CoreV1().Pods(ns).List(context.Background(), alertmanager.ListOptions(alertmanagerName))