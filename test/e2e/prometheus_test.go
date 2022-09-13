prometheus-operator/prometheus-operator

commit 84ffc50f68f619c0cb2e3e6999ea267a259b4635

fix: invalid pod definition due to long volume names (#4988)
Previously, adding secrets to Prometheus resource longer than 50
characters resulted in generation of statefulset that have
`volume.name` larger than 63 characters. This doesn't conform to
RFC-1123 (<= 63 chars) requirement and resulted in pod not getting
created.

This patch fixes the issue by ensuring that the volume name conform to
the requirement. Additionally it fixes an issue with SanitizeVolumeName
which used to truncate the name at DNS1123LabelMaxLength thus returing
same name for `<63-char>-foo` and `<63-char>-bar` to return unique name
by computing the sha1 and appending to the end.

This commit also ensures that secrets and configmaps provided via the
Alertmanager and Prometheus have correct volume mounts.

Bug: https://github.com/prometheus-operator/prometheus-operator/issues/4944

Signed-off-by: Simon Pasquier <spasquie@redhat.com>
Signed-off-by: Sunil Thaha <sthaha@redhat.com>

Signed-off-by: Simon Pasquier <spasquie@redhat.com>
Signed-off-by: Sunil Thaha <sthaha@redhat.com>

pkg/alertmanager/statefulset.go
@@ -512,9 +512,15 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 		},
 	}
 
+	rn := k8sutil.NewResourceNamerWithPrefix("secret")
 	for _, s := range a.Spec.Secrets {
+		name, err := rn.UniqueVolumeName(s)
+		if err != nil {
+			return nil, err
+		}
+
 		volumes = append(volumes, v1.Volume{
-			Name: k8sutil.SanitizeVolumeName("secret-" + s),
+			Name: name,
 			VolumeSource: v1.VolumeSource{
 				Secret: &v1.SecretVolumeSource{
 					SecretName: s,
@@ -523,7 +529,7 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 		})
 		mountPath := secretsDir + s
 		mount := v1.VolumeMount{
-			Name:      k8sutil.SanitizeVolumeName("secret-" + s),
+			Name:      name,
 			ReadOnly:  true,
 			MountPath: mountPath,
 		}
@@ -532,9 +538,15 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 		watchedDirectories = append(watchedDirectories, mountPath)
 	}
 
+	rn = k8sutil.NewResourceNamerWithPrefix("configmap")
 	for _, c := range a.Spec.ConfigMaps {
+		name, err := rn.UniqueVolumeName(c)
+		if err != nil {
+			return nil, err
+		}
+
 		volumes = append(volumes, v1.Volume{
-			Name: k8sutil.SanitizeVolumeName("configmap-" + c),
+			Name: name,
 			VolumeSource: v1.VolumeSource{
 				ConfigMap: &v1.ConfigMapVolumeSource{
 					LocalObjectReference: v1.LocalObjectReference{
@@ -545,7 +557,7 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 		})
 		mountPath := configmapsDir + c
 		mount := v1.VolumeMount{
-			Name:      k8sutil.SanitizeVolumeName("configmap-" + c),
+			Name:      name,
 			ReadOnly:  true,
 			MountPath: mountPath,
 		}
@@ -571,7 +583,10 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 			return nil, err
 		}
 
-		confArg, configVol, configMount := webConfig.GetMountParameters()
+		confArg, configVol, configMount, err := webConfig.GetMountParameters()
+		if err != nil {
+			return nil, err
+		}
 		amArgs = append(amArgs, fmt.Sprintf("--%s=%s", confArg.Name, confArg.Value))
 		volumes = append(volumes, configVol...)
 		amVolumeMounts = append(amVolumeMounts, configMount...)

pkg/k8sutil/k8sutil.go
@@ -16,6 +16,7 @@ package k8sutil
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -23,6 +24,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/cespare/xxhash/v2"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	monitoringv1alpha1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1alpha1"
 	monitoringv1beta1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1beta1"
@@ -232,15 +234,57 @@ func IsAPIGroupVersionResourceSupported(discoveryCli discovery.DiscoveryInterfac
 	return false, nil
 }
 
-// SanitizeVolumeName ensures that the given volume name is a valid DNS-1123 label
-// accepted by Kubernetes.
-func SanitizeVolumeName(name string) string {
+// ResourceNamer knows how to generate valid names for various Kubernetes resources.
+type ResourceNamer struct {
+	prefix string
+}
+
+// NewResourceNamerWithPrefix returns a ResourceNamer that adds a prefix
+// followed by an hyphen character to all resource names.
+func NewResourceNamerWithPrefix(p string) ResourceNamer {
+	return ResourceNamer{prefix: p}
+}
+
+// UniqueVolumeName returns a volume name that is a valid DNS-1123 label.
+// The returned name has a hash-based suffix to ensure uniqueness in case the
+// input name exceeds the 63-chars limit.
+func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
+	if rn.prefix != "" {
+		name = strings.TrimRight(rn.prefix, "-") + "-" + name
+	}
+
 	name = strings.ToLower(name)
 	name = invalidDNS1123Characters.ReplaceAllString(name, "-")
-	if len(name) > validation.DNS1123LabelMaxLength {
-		name = name[0:validation.DNS1123LabelMaxLength]
+	name = strings.Trim(name, "-")
+
+	// Hash the name and append the 8 first characters of the hash
+	// value to the resulting name to ensure that 2 names longer than
+	// DNS1123LabelMaxLength return unique volume names.
+	// E.g. long-63-chars-abc, long-63-chars-XYZ may be added to
+	// volume name since they are trimmed at long-63-chars, there will be 2
+	// volume entries with the same name.
+	// In practice, the hash is computed for the full name then trimmed to
+	// the first 8 chars and added to the end:
+	// * long-63-chars-abc -> first-54-chars-deadbeef
+	// * long-63-chars-XYZ -> first-54-chars-d3adb33f
+	xxh := xxhash.New()
+	if _, err := xxh.Write([]byte(name)); err != nil {
+		return "", err
 	}
-	return strings.Trim(name, "-")
+
+	h := fmt.Sprintf("-%x", xxh.Sum64())
+	h = h[:9]
+
+	if len(name) > validation.DNS1123LabelMaxLength-9 {
+		name = name[:validation.DNS1123LabelMaxLength-9]
+	}
+
+	name = name + h
+	if errs := validation.IsDNS1123Label(name); len(errs) > 0 {
+		return "", errors.New(strings.Join(errs, ","))
+	}
+
+	return name, nil
 }
 
 // AddTypeInformationToObject adds TypeMeta information to a runtime.Object based upon the loaded scheme.Scheme

commit 079e2ecab43c33fd3d9a656b140ae41f4c2e2471

Main 59.1 sync (#5016)
* pkg/prometheus: add CAP_FOWNER capability to Thanos only if needed

When configuring the Thanos sidecar to upload blocks to object storage,
the container needs to the CAP_FOWNER capability. Otherwise it isn't
required. This is a small improvement on top of #4931 to avoid granting
more capabilities than needed.

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

* Fixed secret and configmap volume names that need to be mounted in additional containers (#5000)

* test/e2e: add testPromStrategicMergePatch

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

* *: fix volume names for mounted secrets/configmaps

This change fixes a regression introduced in #4988 (84ffc50).

The volume names for additional secrets/configmaps provided by
`.spec.secrets` and `.spec.configMaps` shouldn't be suffixed by the hash
ID as it is done for the webconfig secrets/configmaps. Otherwise it
is impossible to mount them in a sidecar container since there's no way
to know the volume name in advance.

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

* fix: thanos-sidecar CAP_FOWNER capability

this remove the CAP_ prefix of the FOWNER capability on thanos-sidecar

* 0.59.1 (#5013)

* docs: Update changelog for v0.59.1

* chore: Prep for v0.59.1

Signed-off-by: Simon Pasquier <spasquie@redhat.com>
Co-authored-by: Simon Pasquier <spasquie@redhat.com>
Co-authored-by: Francesco Steccanella <fsteccanella@gmail.com>

VERSION
@@ -1,1 +1,1 @@
-0.59.0
+0.59.1

bundle.yaml
@@ -28702,7 +28702,7 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
 roleRef:
   apiGroup: rbac.authorization.k8s.io
@@ -28719,7 +28719,7 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
 rules:
 - apiGroups:
@@ -28800,7 +28800,7 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
   namespace: default
 spec:
@@ -28816,14 +28816,14 @@ spec:
       labels:
         app.kubernetes.io/component: controller
         app.kubernetes.io/name: prometheus-operator
-        app.kubernetes.io/version: 0.59.0
+        app.kubernetes.io/version: 0.59.1
     spec:
       automountServiceAccountToken: true
       containers:
       - args:
         - --kubelet-service=kube-system/kubelet
-        - --prometheus-config-reloader=quay.io/prometheus-operator/prometheus-config-reloader:v0.59.0
-        image: quay.io/prometheus-operator/prometheus-operator:v0.59.0
+        - --prometheus-config-reloader=quay.io/prometheus-operator/prometheus-config-reloader:v0.59.1
+        image: quay.io/prometheus-operator/prometheus-operator:v0.59.1
         name: prometheus-operator
         ports:
         - containerPort: 8080
@@ -28855,7 +28855,7 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
   namespace: default
 ---
@@ -28865,7 +28865,7 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
   namespace: default
 spec:

example/admission-webhook/deployment.yaml
@@ -3,7 +3,7 @@ kind: Deployment
 metadata:
   labels:
     app.kubernetes.io/name: prometheus-operator-admission-webhook
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator-admission-webhook
   namespace: default
 spec:
@@ -17,11 +17,11 @@ spec:
         kubectl.kubernetes.io/default-container: prometheus-operator-admission-webhook
       labels:
         app.kubernetes.io/name: prometheus-operator-admission-webhook
-        app.kubernetes.io/version: 0.59.0
+        app.kubernetes.io/version: 0.59.1
     spec:
       automountServiceAccountToken: false
       containers:
-      - image: quay.io/prometheus-operator/admission-webhook:v0.59.0
+      - image: quay.io/prometheus-operator/admission-webhook:v0.59.1
         name: prometheus-operator-admission-webhook
         ports:
         - containerPort: 8443

example/admission-webhook/pod-disruption-budget.yaml
@@ -3,7 +3,7 @@ kind: PodDisruptionBudget
 metadata:
   labels:
     app.kubernetes.io/name: prometheus-operator-admission-webhook
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator-admission-webhook
   namespace: default
 spec:

example/admission-webhook/service-account.yaml
@@ -4,6 +4,6 @@ kind: ServiceAccount
 metadata:
   labels:
     app.kubernetes.io/name: prometheus-operator-admission-webhook
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator-admission-webhook
   namespace: default

example/admission-webhook/service-monitor.yaml
@@ -3,7 +3,7 @@ kind: ServiceMonitor
 metadata:
   labels:
     app.kubernetes.io/name: prometheus-operator-admission-webhook
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator-admission-webhook
   namespace: default
 spec:
@@ -13,4 +13,4 @@ spec:
   selector:
     matchLabels:
       app.kubernetes.io/name: prometheus-operator-admission-webhook
-      app.kubernetes.io/version: 0.59.0
+      app.kubernetes.io/version: 0.59.1

example/admission-webhook/service.yaml
@@ -3,7 +3,7 @@ kind: Service
 metadata:
   labels:
     app.kubernetes.io/name: prometheus-operator-admission-webhook
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator-admission-webhook
   namespace: default
 spec:

example/rbac/prometheus-operator/prometheus-operator-cluster-role-binding.yaml
@@ -4,7 +4,7 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
 roleRef:
   apiGroup: rbac.authorization.k8s.io

example/rbac/prometheus-operator/prometheus-operator-cluster-role.yaml
@@ -4,7 +4,7 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
 rules:
 - apiGroups:

example/rbac/prometheus-operator/prometheus-operator-deployment.yaml
@@ -4,7 +4,7 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
   namespace: default
 spec:
@@ -20,14 +20,14 @@ spec:
       labels:
         app.kubernetes.io/component: controller
         app.kubernetes.io/name: prometheus-operator
-        app.kubernetes.io/version: 0.59.0
+        app.kubernetes.io/version: 0.59.1
     spec:
       automountServiceAccountToken: true
       containers:
       - args:
         - --kubelet-service=kube-system/kubelet
-        - --prometheus-config-reloader=quay.io/prometheus-operator/prometheus-config-reloader:v0.59.0
-        image: quay.io/prometheus-operator/prometheus-operator:v0.59.0
+        - --prometheus-config-reloader=quay.io/prometheus-operator/prometheus-config-reloader:v0.59.1
+        image: quay.io/prometheus-operator/prometheus-operator:v0.59.1
         name: prometheus-operator
         ports:
         - containerPort: 8080

example/rbac/prometheus-operator/prometheus-operator-service-account.yaml
@@ -5,6 +5,6 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
   namespace: default

example/rbac/prometheus-operator/prometheus-operator-service-monitor.yaml
@@ -4,7 +4,7 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
   namespace: default
 spec:
@@ -15,4 +15,4 @@ spec:
     matchLabels:
       app.kubernetes.io/component: controller
       app.kubernetes.io/name: prometheus-operator
-      app.kubernetes.io/version: 0.59.0
+      app.kubernetes.io/version: 0.59.1

example/rbac/prometheus-operator/prometheus-operator-service.yaml
@@ -4,7 +4,7 @@ metadata:
   labels:
     app.kubernetes.io/component: controller
     app.kubernetes.io/name: prometheus-operator
-    app.kubernetes.io/version: 0.59.0
+    app.kubernetes.io/version: 0.59.1
   name: prometheus-operator
   namespace: default
 spec:

go.mod
@@ -20,8 +20,8 @@ require (
 	github.com/oklog/run v1.1.0
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus-community/prom-label-proxy v0.5.0
-	github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring v0.59.0
-	github.com/prometheus-operator/prometheus-operator/pkg/client v0.59.0
+	github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring v0.59.1
+	github.com/prometheus-operator/prometheus-operator/pkg/client v0.59.1
 	github.com/prometheus/alertmanager v0.24.0
 	github.com/prometheus/client_golang v1.13.0
 	github.com/prometheus/common v0.37.0

pkg/alertmanager/statefulset.go
@@ -562,7 +562,7 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 
 	rn := k8sutil.NewResourceNamerWithPrefix("secret")
 	for _, s := range a.Spec.Secrets {
-		name, err := rn.UniqueVolumeName(s)
+		name, err := rn.VolumeName(s)
 		if err != nil {
 			return nil, err
 		}
@@ -588,7 +588,7 @@ func makeStatefulSetSpec(a *monitoringv1.Alertmanager, config Config, tlsAssetSe
 
 	rn = k8sutil.NewResourceNamerWithPrefix("configmap")
 	for _, c := range a.Spec.ConfigMaps {
-		name, err := rn.UniqueVolumeName(c)
+		name, err := rn.VolumeName(c)
 		if err != nil {
 			return nil, err
 		}

pkg/client/go.mod
@@ -3,7 +3,7 @@ module github.com/prometheus-operator/prometheus-operator/pkg/client
 go 1.17
 
 require (
-	github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring v0.59.0
+	github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring v0.59.1
 	k8s.io/apimachinery v0.25.0
 	k8s.io/client-go v0.25.0
 )

pkg/k8sutil/k8sutil.go
@@ -245,10 +245,7 @@ func NewResourceNamerWithPrefix(p string) ResourceNamer {
 	return ResourceNamer{prefix: p}
 }
 
-// UniqueVolumeName returns a volume name that is a valid DNS-1123 label.
-// The returned name has a hash-based suffix to ensure uniqueness in case the
-// input name exceeds the 63-chars limit.
-func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
+func (rn ResourceNamer) sanitizedVolumeName(name string) string {
 	if rn.prefix != "" {
 		name = strings.TrimRight(rn.prefix, "-") + "-" + name
 	}
@@ -257,6 +254,21 @@ func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
 	name = invalidDNS1123Characters.ReplaceAllString(name, "-")
 	name = strings.Trim(name, "-")
 
+	return name
+}
+
+func isValidDNS1123Label(name string) error {
+	if errs := validation.IsDNS1123Label(name); len(errs) > 0 {
+		return errors.New(strings.Join(errs, ","))
+	}
+
+	return nil
+}
+
+// UniqueVolumeName returns a volume name that is a valid DNS-1123 label.
+// The returned name has a hash-based suffix to ensure uniqueness in case the
+// input name exceeds the 63-chars limit.
+func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
 	// Hash the name and append the 8 first characters of the hash
 	// value to the resulting name to ensure that 2 names longer than
 	// DNS1123LabelMaxLength return unique volume names.
@@ -275,6 +287,8 @@ func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
 	h := fmt.Sprintf("-%x", xxh.Sum64())
 	h = h[:9]
 
+	name = rn.sanitizedVolumeName(name)
+
 	if len(name) > validation.DNS1123LabelMaxLength-9 {
 		name = name[:validation.DNS1123LabelMaxLength-9]
 	}
@@ -284,7 +298,18 @@ func (rn ResourceNamer) UniqueVolumeName(name string) (string, error) {
 		return "", errors.New(strings.Join(errs, ","))
 	}
 
-	return name, nil
+	return name, isValidDNS1123Label(name)
+}
+
+// VolumeName returns a volume name that is a valid DNS-1123 label.
+func (rn ResourceNamer) VolumeName(name string) (string, error) {
+	name = rn.sanitizedVolumeName(name)
+
+	if len(name) > validation.DNS1123LabelMaxLength {
+		name = name[:validation.DNS1123LabelMaxLength]
+	}
+
+	return name, isValidDNS1123Label(name)
 }
 
 // AddTypeInformationToObject adds TypeMeta information to a runtime.Object based upon the loaded scheme.Scheme

pkg/prometheus/statefulset.go
@@ -582,7 +582,7 @@ func makeStatefulSetSpec(
 
 	rn := k8sutil.NewResourceNamerWithPrefix("secret")
 	for _, s := range p.Spec.Secrets {
-		name, err := rn.UniqueVolumeName(s)
+		name, err := rn.VolumeName(s)
 		if err != nil {
 			return nil, err
 		}
@@ -604,7 +604,7 @@ func makeStatefulSetSpec(
 
 	rn = k8sutil.NewResourceNamerWithPrefix("configmap")
 	for _, c := range p.Spec.ConfigMaps {
-		name, err := rn.UniqueVolumeName(c)
+		name, err := rn.VolumeName(c)
 		if err != nil {
 			return nil, err
 		}
@@ -779,8 +779,6 @@ func makeStatefulSetSpec(
 				AllowPrivilegeEscalation: &boolFalse,
 				ReadOnlyRootFilesystem:   &boolTrue,
 				Capabilities: &v1.Capabilities{
-					// The Thanos sidecar needs the CAP_FOWNER capability because it links block files as hard link.
-					Add:  []v1.Capability{"CAP_FOWNER"},
 					Drop: []v1.Capability{"ALL"},
 				},
 			},
@@ -825,6 +823,9 @@ func makeStatefulSetSpec(
 					SubPath:   subPathForStorage(p.Spec.Storage),
 				},
 			)
+
+			// The Thanos sidecar needs the FOWNER capability because it links block files as hard link.
+			container.SecurityContext.Capabilities.Add = append(container.SecurityContext.Capabilities.Add, "FOWNER")
 
 			// NOTE(bwplotka): As described in https://thanos.io/components/sidecar.md/ we have to turn off compaction of Prometheus
 			// to avoid races during upload, if the uploads are configured.

pkg/alertmanager/statefulset_test.go
@@ -616,10 +616,10 @@ func TestAdditionalSecretsMounted(t *testing.T) {
 	secret1Found = false
 	secret2Found = false
 	for _, v := range sset.Spec.Template.Spec.Containers[0].VolumeMounts {
-		if strings.HasPrefix(v.Name, "secret-secret1-") && v.MountPath == "/etc/alertmanager/secrets/secret1" {
+		if v.Name == "secret-secret1" && v.MountPath == "/etc/alertmanager/secrets/secret1" {
 			secret1Found = true
 		}
-		if strings.HasPrefix(v.Name, "secret-secret2-") && v.MountPath == "/etc/alertmanager/secrets/secret2" {
+		if v.Name == "secret-secret2" && v.MountPath == "/etc/alertmanager/secrets/secret2" {
 			secret2Found = true
 		}
 	}
@@ -769,7 +769,7 @@ func TestAdditionalConfigMap(t *testing.T) {
 
 	cmVolumeFound := false
 	for _, v := range sset.Spec.Template.Spec.Volumes {
-		if strings.HasPrefix(v.Name, "configmap-test-cm1-") {
+		if v.Name == "configmap-test-cm1" {
 			cmVolumeFound = true
 			break
 		}
@@ -780,7 +780,7 @@ func TestAdditionalConfigMap(t *testing.T) {
 
 	cmMounted := false
 	for _, v := range sset.Spec.Template.Spec.Containers[0].VolumeMounts {
-		if strings.HasPrefix(v.Name, "configmap-test-cm1-") && v.MountPath == "/etc/alertmanager/configmaps/test-cm1" {
+		if v.Name == "configmap-test-cm1" && v.MountPath == "/etc/alertmanager/configmaps/test-cm1" {
 			cmMounted = true
 			break
 		}

pkg/k8sutil/k8sutil_test.go
@@ -43,19 +43,19 @@ func TestUniqueVolumeName(t *testing.T) {
 		},
 		{
 			name:     "NAME",
-			expected: "name-6c5f7b2e",
+			expected: "name-4cfd3574",
 		},
 		{
 			name:     "foo--",
-			expected: "foo-33bf00a8",
+			expected: "foo-e705c7c8",
 		},
 		{
 			name:     "foo^%#$bar",
-			expected: "foo-bar-6ab521af",
+			expected: "foo-bar-f3e212b1",
 		},
 		{
 			name:     "fOo^%#$bar",
-			expected: "foo-bar-6ab521af",
+			expected: "foo-bar-ee5c3c18",
 		},
 		{
 			name: strings.Repeat("a", validation.DNS1123LabelMaxLength*2),
@@ -65,18 +65,17 @@ func TestUniqueVolumeName(t *testing.T) {
 		{
 			prefix:   "with-prefix",
 			name:     "name",
-			expected: "with-prefix-name-fa429965",
+			expected: "with-prefix-name-6c5f7b2e",
 		},
 		{
 			prefix:   "with-prefix-",
 			name:     "name",
-			expected: "with-prefix-name-fa429965",
+			expected: "with-prefix-name-6c5f7b2e",
 		},
 		{
-			prefix: "with-prefix",
-			name:   strings.Repeat("a", validation.DNS1123LabelMaxLength*2),
-			expected: "with-prefix-" + strings.Repeat("a", 42) +
-				"-c183166e",
+			prefix:   "with-prefix",
+			name:     strings.Repeat("a", validation.DNS1123LabelMaxLength*2),
+			expected: "with-prefix-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-4ed69ce2",
 		},
 	}
 

pkg/prometheus/statefulset_test.go
@@ -316,7 +316,7 @@ func TestStatefulSetVolumeInitial(t *testing.T) {
 									SubPath:   "web-config.yaml",
 								},
 								{
-									Name:      "secret-test-secret1-df14634a",
+									Name:      "secret-test-secret1",
 									ReadOnly:  true,
 									MountPath: "/etc/prometheus/secrets/test-secret1",
 									SubPath:   "",
@@ -374,7 +374,7 @@ func TestStatefulSetVolumeInitial(t *testing.T) {
 							},
 						},
 						{
-							Name: "secret-test-secret1-df14634a",
+							Name: "secret-test-secret1",
 							VolumeSource: v1.VolumeSource{
 								Secret: &v1.SecretVolumeSource{
 									SecretName: "test-secret1",
@@ -1050,9 +1050,9 @@ func TestThanosNoObjectStorage(t *testing.T) {
 		}
 	}
 
-	for _, vol := range sset.Spec.Template.Spec.Containers[2].VolumeMounts {
-		if vol.MountPath == storageDir {
-			t.Fatal("Prometheus data volume should not be mounted in the Thanos sidecar")
+	for _, addCap := range sset.Spec.Template.Spec.Containers[2].SecurityContext.Capabilities.Add {
+		if addCap == "FOWNER" {
+			t.Fatal("Thanos sidecar shouldn't have the FOWNER capability")
 		}
 	}
 }
@@ -1144,6 +1144,19 @@ func TestThanosObjectStorage(t *testing.T) {
 		}
 		if !found {
 			t.Fatal("Prometheus data volume should be mounted in the Thanos sidecar")
+		}
+	}
+
+	{
+		var found bool
+		for _, addCap := range sset.Spec.Template.Spec.Containers[2].SecurityContext.Capabilities.Add {
+			if addCap == "FOWNER" {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Fatal("Thanos sidecar should have FOWNER capability")
 		}
 	}
 }

pkg/webconfig/config_test.go
@@ -293,7 +293,7 @@ func TestGetMountParameters(t *testing.T) {
 					},
 				},
 				{
-					Name: "web-config-tls-secret-key-some-secret-38b2f493",
+					Name: "web-config-tls-secret-key-some-secret-3556f148",
 					VolumeSource: v1.VolumeSource{
 						Secret: &v1.SecretVolumeSource{
 							SecretName: "some-secret",
@@ -301,7 +301,7 @@ func TestGetMountParameters(t *testing.T) {
 					},
 				},
 				{
-					Name: "web-config-tls-secret-cert-some-secret-a60e0a56",
+					Name: "web-config-tls-secret-cert-some-secret-3556f148",
 					VolumeSource: v1.VolumeSource{
 						Secret: &v1.SecretVolumeSource{
 							SecretName: "some-secret",
@@ -309,7 +309,7 @@ func TestGetMountParameters(t *testing.T) {
 					},
 				},
 				{
-					Name: "web-config-tls-secret-client-ca-some-secret-3ec4509b",
+					Name: "web-config-tls-secret-client-ca-some-secret-3556f148",
 					VolumeSource: v1.VolumeSource{
 						Secret: &v1.SecretVolumeSource{
 							SecretName: "some-secret",
@@ -327,7 +327,7 @@ func TestGetMountParameters(t *testing.T) {
 					SubPathExpr:      "",
 				},
 				{
-					Name:             "web-config-tls-secret-key-some-secret-38b2f493",
+					Name:             "web-config-tls-secret-key-some-secret-3556f148",
 					ReadOnly:         true,
 					MountPath:        "/etc/prometheus/web_config/secret_some-secret_tls.key",
 					SubPath:          "tls.key",
@@ -335,7 +335,7 @@ func TestGetMountParameters(t *testing.T) {
 					SubPathExpr:      "",
 				},
 				{
-					Name:             "web-config-tls-secret-cert-some-secret-a60e0a56",
+					Name:             "web-config-tls-secret-cert-some-secret-3556f148",
 					ReadOnly:         true,
 					MountPath:        "/etc/prometheus/web_config/secret_some-secret_tls.crt",
 					SubPath:          "tls.crt",
@@ -343,7 +343,7 @@ func TestGetMountParameters(t *testing.T) {
 					SubPathExpr:      "",
 				},
 				{
-					Name:             "web-config-tls-secret-client-ca-some-secret-3ec4509b",
+					Name:             "web-config-tls-secret-client-ca-some-secret-3556f148",
 					ReadOnly:         true,
 					MountPath:        "/etc/prometheus/web_config/secret_some-secret_tls.client_ca",
 					SubPath:          "tls.client_ca",

test/e2e/main_test.go
@@ -222,6 +222,7 @@ func testAllNSPrometheus(t *testing.T) {
 		"PromNamespaceEnforcementExclusion":      testPromNamespaceEnforcementExclusion,
 		"PromQueryLogFile":                       testPromQueryLogFile,
 		"PromDegradedCondition":                  testPromDegradedConditionStatus,
+		"PromStrategicMergePatch":                testPromStrategicMergePatch,
 	}
 
 	for name, f := range testFuncs {

test/e2e/prometheus_test.go
@@ -4486,6 +4486,53 @@ func testPromDegradedConditionStatus(t *testing.T) {
 	}
 }
 
+func testPromStrategicMergePatch(t *testing.T) {
+	t.Parallel()
+	testCtx := framework.NewTestCtx(t)
+	defer testCtx.Cleanup(t)
+	ns := framework.CreateNamespace(context.Background(), t, testCtx)
+	framework.SetupPrometheusRBAC(context.Background(), t, testCtx, ns)
+
+	secret := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: "secret", Namespace: ns},
+		Type:       v1.SecretType("Opaque"),
+		Data:       map[string][]byte{},
+	}
+	_, err := framework.KubeClient.CoreV1().Secrets(ns).Create(context.Background(), secret, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("failed to create secret: %s", err)
+	}
+
+	configmap := &v1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{Name: "configmap", Namespace: ns},
+		Data:       map[string]string{},
+	}
+	_, err = framework.KubeClient.CoreV1().ConfigMaps(ns).Create(context.Background(), configmap, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("failed to create configmap: %s", err)
+	}
+
+	p := framework.MakeBasicPrometheus(ns, "test", "", 1)
+	p.Spec.Secrets = []string{secret.Name}
+	p.Spec.ConfigMaps = []string{configmap.Name}
+	p.Spec.Containers = []v1.Container{{
+		Name:  "sidecar",
+		Image: "nginx",
+		// Ensure that the sidecar container can mount the additional secret and configmap.
+		VolumeMounts: []v1.VolumeMount{{
+			Name:      "secret-" + secret.Name,
+			MountPath: "/tmp/secret",
+		}, {
+			Name:      "configmap-" + configmap.Name,
+			MountPath: "/tmp/configmap",
+		}},
+	}}
+
+	if _, err := framework.CreatePrometheusAndWaitUntilReady(context.Background(), ns, p); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func isAlertmanagerDiscoveryWorking(ctx context.Context, ns, promSVCName, alertmanagerName string) func() (bool, error) {
 	return func() (bool, error) {
 		pods, err := framework.KubeClient.CoreV1().Pods(ns).List(context.Background(), alertmanager.ListOptions(alertmanagerName))