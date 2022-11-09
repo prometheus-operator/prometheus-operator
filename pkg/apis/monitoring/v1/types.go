5649aee87ae0a8e42066d61594efdef766787e4a
Razvan Dobre <dobre.razvan@gmail.com>
2022-11-09 10:27:45 +0200

    Update pkg/apis/monitoring/v1/types.go
    Co-authored-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/apis/monitoring/v1/types.go
+++ pkg/apis/monitoring/v1/types.go
@@ -1124,7 +1124,7 @@ type RemoteReadSpec struct {
 	ProxyURL string `json:"proxyUrl,omitempty"`
 	// Whether to use the external labels as selectors for the remote read endpoint.
 	// Requires Prometheus v2.34.0 and above.
-	FilterExternalLabels bool `json:"filterExternalLabels,omitempty"`
+	FilterExternalLabels *bool `json:"filterExternalLabels,omitempty"`
 }
 
 // LabelName is a valid Prometheus label name which may only contain ASCII letters, numbers, as well as underscores.
