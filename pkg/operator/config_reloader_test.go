306bc970c450ec61d6b4f6c77d0e067a6fce2b0f
dongjiang <dongjiang1989@126.com>
2023-04-07 18:20:02 +0800

    Update pkg/operator/config_reloader_test.go
    Co-authored-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/operator/config_reloader_test.go
+++ pkg/operator/config_reloader_test.go
@@ -80,7 +80,7 @@ func TestCreateInitConfigReloaderEnableProbes(t *testing.T) {
 	}
 
 	if container.LivenessProbe != nil {
-		t.Errorf("Expected LivenessProbe %v, but found %s", nil, container.LivenessProbe)
+		t.Errorf("expected no LivenessProbe but got %v", container.LivenessProbe)
 	}
 
 	if container.ReadinessProbe != nil {
