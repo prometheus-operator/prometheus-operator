f1481156e925e44475ca7cff9d06ef71481d49c0
Azanul <azanulhaque@gmail.com>
2023-06-13 21:01:41 +0530

    make generate
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- pkg/client/applyconfiguration/utils.go
+++ pkg/client/applyconfiguration/utils.go
@@ -185,6 +185,8 @@ func ForKind(kind schema.GroupVersionKind) interface{} {
 		return &monitoringv1alpha1.AlertmanagerConfigSpecApplyConfiguration{}
 	case v1alpha1.SchemeGroupVersion.WithKind("DayOfMonthRange"):
 		return &monitoringv1alpha1.DayOfMonthRangeApplyConfiguration{}
+	case v1alpha1.SchemeGroupVersion.WithKind("DiscordConfig"):
+		return &monitoringv1alpha1.DiscordConfigApplyConfiguration{}
 	case v1alpha1.SchemeGroupVersion.WithKind("EmailConfig"):
 		return &monitoringv1alpha1.EmailConfigApplyConfiguration{}
 	case v1alpha1.SchemeGroupVersion.WithKind("FileSDConfig"):
@@ -257,6 +259,8 @@ func ForKind(kind schema.GroupVersionKind) interface{} {
 		return &monitoringv1beta1.AlertmanagerConfigSpecApplyConfiguration{}
 	case v1beta1.SchemeGroupVersion.WithKind("DayOfMonthRange"):
 		return &monitoringv1beta1.DayOfMonthRangeApplyConfiguration{}
+	case v1beta1.SchemeGroupVersion.WithKind("DiscordConfig"):
+		return &monitoringv1beta1.DiscordConfigApplyConfiguration{}
 	case v1beta1.SchemeGroupVersion.WithKind("EmailConfig"):
 		return &monitoringv1beta1.EmailConfigApplyConfiguration{}
 	case v1beta1.SchemeGroupVersion.WithKind("HTTPConfig"):
