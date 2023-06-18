62d67c977b1bf7c700f042a907d8d736082bf484
Bruno Bressi <bruno.bressi@telekom.de>
2023-06-18 12:27:25 +0200

    fix: lenght check is done properly now

--- test/e2e/scrapeconfig_test.go
+++ test/e2e/scrapeconfig_test.go
@@ -215,7 +215,7 @@ func testPromOperatorStartsWithoutScrapeConfigCRD(t *testing.T) {
 
 	restarts, err := framework.GetPodRestartCount(context.Background(), ns, pl.Items[0].GetName())
 	require.NoError(t, err)
-	require.Emptyf(t, restarts, "expected to get 1 container but got %d", len(restarts))
+	require.Lenf(t, restarts, 1, "expected to get 1 container but got %d", len(restarts))
 
 	for _, restart := range restarts {
 		require.Emptyf(t, restart, "expected Prometheus Operator to never restart during entire test execution but got %d restarts", restart)
