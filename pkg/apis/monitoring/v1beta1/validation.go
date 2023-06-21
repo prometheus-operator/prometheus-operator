f6bda1de5b22184f04853cd32d275c6cbb039e45
Bruno Bressi <bruno.bressi@telekom.de>
2023-06-21 20:42:54 +0200

    feat: URL validation

--- pkg/apis/monitoring/v1beta1/validation.go
+++ pkg/apis/monitoring/v1beta1/validation.go
@@ -346,3 +346,12 @@ func parseRange(in string) (start, end string, err error) {
 	}
 	return parts[0], parts[1], nil
 }
+
+// Validate the URL
+func (u URL) Validate() error {
+	regex := regexp.MustCompile(`^https?://.+$`)
+	if !regex.MatchString(string(u)) {
+		return fmt.Errorf("invalid url: %s", u)
+	}
+	return nil
+}
