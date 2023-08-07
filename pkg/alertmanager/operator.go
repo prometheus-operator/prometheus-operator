abcbd9abe13c18d99a843529942911b8f1e6be4e
Azanul Haque <42029519+Azanul@users.noreply.github.com>
2023-08-07 14:43:28 +0530

    Update pkg/alertmanager/operator.go
    Co-authored-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/alertmanager/operator.go
+++ pkg/alertmanager/operator.go
@@ -821,7 +821,9 @@ func createSSetInputHash(a monitoringv1.Alertmanager, c Config, tlsAssets *opera
 		http2 = a.Spec.Web.WebConfigFileFields.HTTPConfig.HTTP2
 	}
 
-	// Ignoring changes to RevisionHistoryLimit field
+	// The controller should ignore any changes to RevisionHistoryLimit field because 
+	// it may be modified by external actors.
+	// See https://github.com/prometheus-operator/prometheus-operator/issues/5712
 	s.RevisionHistoryLimit = nil
 
 	hash, err := hashstructure.Hash(struct {
