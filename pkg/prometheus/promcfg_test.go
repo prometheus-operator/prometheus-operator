003bbd90881960a8b5ac5bc57ce96d5320d9e89d
Nicolas Takashi <nicolas.tcs@hotmail.com>
2023-08-30 15:06:32 +0100

    [CHORE] adding TestAlertmanagerBasicAuth goldenfiles
    Signed-off-by: Nicolas Takashi <nicolas.tcs@hotmail.com>

--- pkg/prometheus/promcfg_test.go
+++ pkg/prometheus/promcfg_test.go
@@ -896,75 +896,17 @@ func TestAlertmanagerBasicAuth(t *testing.T) {
 		name           string
 		version        string
 		expectedConfig string
+		golden         string
 	}{
 		{
 			name:    "Valid Prom Version",
 			version: "2.26.0",
-			expectedConfig: `global:
-  evaluation_interval: 30s
-  scrape_interval: 30s
-  external_labels:
-    prometheus: default/test
-    prometheus_replica: $(POD_NAME)
-scrape_configs: []
-alerting:
-  alert_relabel_configs:
-  - action: labeldrop
-    regex: prometheus_replica
-  alertmanagers:
-  - path_prefix: /
-    scheme: http
-    kubernetes_sd_configs:
-    - role: endpoints
-      namespaces:
-        names:
-        - default
-    basic_auth:
-      username: bob
-      password: alice
-    relabel_configs:
-    - action: keep
-      source_labels:
-      - __meta_kubernetes_service_name
-      regex: alertmanager-main
-    - action: keep
-      source_labels:
-      - __meta_kubernetes_endpoint_port_name
-      regex: web
-`,
+			golden:  "AlertmanagerBasicAuth_Valid_Prom_Version.golden",
 		},
 		{
 			name:    "Invalid Prom Version",
 			version: "2.25.0",
-			expectedConfig: `global:
-  evaluation_interval: 30s
-  scrape_interval: 30s
-  external_labels:
-    prometheus: default/test
-    prometheus_replica: $(POD_NAME)
-scrape_configs: []
-alerting:
-  alert_relabel_configs:
-  - action: labeldrop
-    regex: prometheus_replica
-  alertmanagers:
-  - path_prefix: /
-    scheme: http
-    kubernetes_sd_configs:
-    - role: endpoints
-      namespaces:
-        names:
-        - default
-    relabel_configs:
-    - action: keep
-      source_labels:
-      - __meta_kubernetes_service_name
-      regex: alertmanager-main
-    - action: keep
-      source_labels:
-      - __meta_kubernetes_endpoint_port_name
-      regex: web
-`,
+			golden:  "AlertmanagerBasicAuth_Invalid_Prom_Version.golden",
 		},
 	} {
 
@@ -1035,7 +977,7 @@ alerting:
 			t.Fatal(err)
 		}
 
-		require.Equal(t, tc.expectedConfig, string(cfg))
+		golden.Assert(t, string(cfg), tc.golden)
 	}
 }
 
