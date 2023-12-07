f9b7643649162ada0d2c24bddabf112f12e89d7a
Nicolas Takashi <nicolas.tcs@hotmail.com>
2023-12-07 09:40:05 +0000

    Update pkg/apis/monitoring/v1/prometheus_types.go
    Co-authored-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/apis/monitoring/v1/prometheus_types.go
+++ pkg/apis/monitoring/v1/prometheus_types.go
@@ -644,7 +644,7 @@ type CommonPrometheusFields struct {
 	// +optional
 	ReloadStrategy *ReloadStrategyType `json:"reloadStrategy,omitempty"`
 
-	// Defines the maximum time that the `prometheus` container's startup probe will wait before being considered failed.
+	// Defines the maximum time that the `prometheus` container's startup probe will wait before being considered failed. The startup probe will return success after the WAL replay is complete. 
 	// If unset, it will be equal to the default value of 600 seconds (= 15 minutes).
 	// If set to 0, it will be equal to the default value of 600 seconds (= 15 minutes).
 	// If set to less than 60 seconds, it will be equal to the default value of 600 seconds (= 15 minutes).
