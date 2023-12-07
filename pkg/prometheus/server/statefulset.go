2968cdc8a534f3e6518d4a384ae30c2d1e7cb9c6
Nicolas Takashi <nicolas.tcs@hotmail.com>
2023-12-07 08:36:45 +0000

    Update statefulset.go
    Co-authored-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/prometheus/server/statefulset.go
+++ pkg/prometheus/server/statefulset.go
@@ -463,11 +463,6 @@ func makeStatefulSetSpec(
 		return nil, fmt.Errorf("failed to merge containers spec: %w", err)
 	}
 
-	// tscs := make([]v1.TopologySpreadConstraint, 0, len(cpf.TopologySpreadConstraints))
-	// for _, tsc := range cpf.TopologySpreadConstraints {
-	// 	tscs = append(tscs, prompkg.MakeK8sTopologySpreadConstraint(finalSelectorLabels, tsc))
-	// }
-
 	// PodManagementPolicy is set to Parallel to mitigate issues in kubernetes: https://github.com/kubernetes/kubernetes/issues/60164
 	// This is also mentioned as one of limitations of StatefulSets: https://kubernetes.io/docs/concepts/workloads/controllers/statefulset/#limitations
 	return &appsv1.StatefulSetSpec{
