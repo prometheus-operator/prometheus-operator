5b949e9e5a05f3573a1e3b166f70731ffd9ad28d
Nicolas Takashi <nicolas.tcs@hotmail.com>
2024-02-12 16:12:18 +0000

    Update pkg/prometheus/store.go
    Co-authored-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/prometheus/store.go
+++ pkg/prometheus/store.go
@@ -106,7 +106,7 @@ func AddAPIServerConfigToStore(ctx context.Context, store *assets.Store, namespa
 func AddScrapeClassesToStore(ctx context.Context, store *assets.Store, namespace string, scrapeClasses []monv1.ScrapeClass) error {
 	for i, scrapeClass := range scrapeClasses {
 		if err := store.AddTLSConfig(ctx, namespace, scrapeClass.TLSConfig); err != nil {
-			return fmt.Errorf("scrape class %d: %w", i, err)
+			return fmt.Errorf("scrape class %q: %w", scrapeClass.Name, err)
 		}
 	}
 	return nil
