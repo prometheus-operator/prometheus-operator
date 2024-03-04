9d2325a0d6f7782dcbbafa088825742fcaa370f4
dongjiang <dongjiang1989@126.com>
2024-03-04 14:24:28 +0800

    Update test/framework/framework.go

--- test/framework/framework.go
+++ test/framework/framework.go
@@ -88,6 +88,7 @@ func New(kubeconfig, opImage, exampleDir, resourcesDir string, operatorVersion s
 		return nil, fmt.Errorf("build config from flags failed: %w", err)
 	}
 
+	// setting DefaultQPS and DefaultBurst
 	if config.QPS < DefaultQPS {
 		config.QPS = DefaultQPS
 	}
