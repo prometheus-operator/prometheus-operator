0151d2aeb01ea5ec90055407f65c717d1c2b4e41
haanhvu <haanh6594@gmail.com>
2024-03-26 01:16:33 +0700

    Fix e2e failture

--- pkg/apis/monitoring/v1/prometheus_types.go
+++ pkg/apis/monitoring/v1/prometheus_types.go
@@ -1497,7 +1497,7 @@ type RelabelConfig struct {
 	SourceLabels []LabelName `json:"sourceLabels,omitempty"`
 
 	// Separator is the string between concatenated SourceLabels.
-	Separator *string `json:"separator"`
+	Separator *string `json:"separator,omitempty"`
 
 	// Label to which the resulting string is written in a replacement.
 	//
