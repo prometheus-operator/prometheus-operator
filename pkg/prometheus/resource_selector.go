b437ff193f2d0d1ad397aa94ed4bc63e38e98805
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2024-04-12 12:31:16 +0530

    remove comments

--- pkg/prometheus/resource_selector.go
+++ pkg/prometheus/resource_selector.go
@@ -249,11 +249,6 @@ func validateScrapeIntervalAndTimeout(p monitoringv1.PrometheusInterface, scrape
 
 func validateRelabelConfigs(p monitoringv1.PrometheusInterface, rcs []monitoringv1.RelabelConfig) error {
 	for i, rc := range rcs {
-		// No need for this now
-		//	if rc == nil {
-		//		return fmt.Errorf("null relabel config")
-		//	}
-
 		if err := validateRelabelConfig(p, rc); err != nil {
 			return fmt.Errorf("[%d]: %w", i, err)
 		}
