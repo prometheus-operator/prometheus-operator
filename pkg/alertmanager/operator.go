b0ede6d77aaef37a8b015f3e16de3105d2d09688
Codeknight03 <kanishkramwani6@gmail.com>
2024-05-14 03:48:49 +0530

    validation: added api url validation for Slack and Discord to stop hanging up the operator.

--- pkg/alertmanager/operator.go
+++ pkg/alertmanager/operator.go
@@ -1193,6 +1193,13 @@ func checkPagerDutyConfigs(
 			}
 		}
 
+		if config.URL != "" {
+			if _, err := validation.ValidateURL(strings.TrimSpace(config.URL)); err != nil {
+				return fmt.Errorf("pagerduty 'url' %s invalid: %w", config.URL, err)
+			}
+
+		}
+
 		if err := configureHTTPConfigInStore(ctx, config.HTTPConfig, namespace, store); err != nil {
 			return err
 		}
@@ -1264,9 +1271,14 @@ func checkDiscordConfigs(
 			return err
 		}
 
-		if _, err := store.GetSecretKey(ctx, namespace, config.APIURL); err != nil {
-			return fmt.Errorf("failed to retrieve API URL: %w", err)
+		url, err := store.GetSecretKey(ctx, namespace, config.APIURL)
+		if err != nil {
+			return err
 		}
+		if _, err := validation.ValidateURL(strings.TrimSpace(url)); err != nil {
+			return fmt.Errorf("discord webhook 'url' %s invalid: %w", url, err)
+		}
+
 	}
 
 	return nil
@@ -1285,9 +1297,14 @@ func checkSlackConfigs(
 		}
 
 		if config.APIURL != nil {
-			if _, err := store.GetSecretKey(ctx, namespace, *config.APIURL); err != nil {
+			url, err := store.GetSecretKey(ctx, namespace, *config.APIURL)
+			if err != nil {
 				return err
 			}
+			if _, err := validation.ValidateURL(strings.TrimSpace(url)); err != nil {
+				return fmt.Errorf("slack api 'url' %s invalid: %w", url, err)
+			}
+
 		}
 
 		if err := configureHTTPConfigInStore(ctx, config.HTTPConfig, namespace, store); err != nil {
