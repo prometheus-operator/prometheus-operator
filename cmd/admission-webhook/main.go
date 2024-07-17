7b7b3a6476d435da8a019169b06d07a9ae70f2fd
perebaj <perebaj@gmail.com>
2024-07-17 16:54:03 -0300

    replace go kit log

--- cmd/admission-webhook/main.go
+++ cmd/admission-webhook/main.go
@@ -24,7 +24,6 @@ import (
 	"syscall"
 
 	"github.com/go-kit/log"
-	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/collectors/version"
@@ -61,7 +60,15 @@ func main() {
 		return
 	}
 
-	logger, err := logging.NewLogger(logConfig)
+	logger, err := logging.NewLoggerSlog(logConfig)
+	if err != nil {
+		stdlog.Fatal(err)
+	}
+
+	// We're currently migrating our logging library from go-kit to slog.
+	// The go-kit logger is being removed in small PRs. For now, we are creating 2 loggers to avoid breaking changes and
+	// to have a smooth transition.
+	goKitLogger, err := logging.NewLogger(logConfig)
 	if err != nil {
 		stdlog.Fatal(err)
 	}
@@ -74,7 +81,7 @@ func main() {
 	wg, ctx := errgroup.WithContext(ctx)
 
 	mux := http.NewServeMux()
-	admit := admission.New(log.With(logger, "component", "admissionwebhook"))
+	admit := admission.New(log.With(goKitLogger, "component", "admissionwebhook"))
 	admit.Register(mux)
 
 	r := prometheus.NewRegistry()
@@ -90,9 +97,9 @@ func main() {
 		w.Write([]byte(`{"status":"up"}`))
 	})
 
-	srv, err := server.NewServer(logger, &serverConfig, mux)
+	srv, err := server.NewServer(goKitLogger, &serverConfig, mux)
 	if err != nil {
-		level.Error(logger).Log("msg", "failed to create web server", "err", err)
+		logger.Error("failed to create web server", "err", err)
 		os.Exit(1)
 	}
 
@@ -105,17 +112,17 @@ func main() {
 
 	select {
 	case sig := <-term:
-		level.Info(logger).Log("msg", "Received signal, exiting gracefully...", "signal", sig.String())
+		logger.Info("Received signal, exiting gracefully...", "signal", sig.String())
 	case <-ctx.Done():
 	}
 
 	if err := srv.Shutdown(ctx); err != nil {
-		level.Warn(logger).Log("msg", "Server shutdown error", "err", err)
+		logger.Warn("Server shutdown error", "err", err)
 	}
 
 	cancel()
 	if err := wg.Wait(); err != nil {
-		level.Warn(logger).Log("msg", "Unhandled error received. Exiting...", "err", err)
+		logger.Warn("Unhandled error received. Exiting...", "err", err)
 		os.Exit(1)
 	}
 }
