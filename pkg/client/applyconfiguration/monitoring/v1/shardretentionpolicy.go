lanzhiwang/prometheus-operator-prometheus-operator

Add ShardRetentionPolicy to Prometheus API
Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

pkg/client/applyconfiguration/monitoring/v1/shardretentionpolicy.go
@@ -1,0 +1,41 @@
+// Copyright The prometheus-operator Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Code generated by applyconfiguration-gen. DO NOT EDIT.
+
+package v1
+
+import (
+	v1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+)
+
+// ShardRetentionPolicyApplyConfiguration represents an declarative configuration of the ShardRetentionPolicy type for use
+// with apply.
+type ShardRetentionPolicyApplyConfiguration struct {
+	WhenScaledDown *v1.WhenScaledDownRetentionType `json:"whenScaledDown,omitempty"`
+}
+
+// ShardRetentionPolicyApplyConfiguration constructs an declarative configuration of the ShardRetentionPolicy type for use with
+// apply.
+func ShardRetentionPolicy() *ShardRetentionPolicyApplyConfiguration {
+	return &ShardRetentionPolicyApplyConfiguration{}
+}
+
+// WithWhenScaledDown sets the WhenScaledDown field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the WhenScaledDown field is set to the value of the last call.
+func (b *ShardRetentionPolicyApplyConfiguration) WithWhenScaledDown(value v1.WhenScaledDownRetentionType) *ShardRetentionPolicyApplyConfiguration {
+	b.WhenScaledDown = &value
+	return b
+}

prometheus-operator/prometheus-operator

pkg/client/applyconfiguration/monitoring/v1/shardretentionpolicy.go
@@ -1,0 +1,41 @@
+// Copyright The prometheus-operator Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Code generated by applyconfiguration-gen. DO NOT EDIT.
+
+package v1
+
+import (
+	v1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+)
+
+// ShardRetentionPolicyApplyConfiguration represents an declarative configuration of the ShardRetentionPolicy type for use
+// with apply.
+type ShardRetentionPolicyApplyConfiguration struct {
+	WhenScaled *v1.WhenScaledRetentionType `json:"whenScaledDown,omitempty"`
+}
+
+// ShardRetentionPolicyApplyConfiguration constructs an declarative configuration of the ShardRetentionPolicy type for use with
+// apply.
+func ShardRetentionPolicy() *ShardRetentionPolicyApplyConfiguration {
+	return &ShardRetentionPolicyApplyConfiguration{}
+}
+
+// WithWhenScaled sets the WhenScaled field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the WhenScaled field is set to the value of the last call.
+func (b *ShardRetentionPolicyApplyConfiguration) WithWhenScaled(value v1.WhenScaledRetentionType) *ShardRetentionPolicyApplyConfiguration {
+	b.WhenScaled = &value
+	return b
+}