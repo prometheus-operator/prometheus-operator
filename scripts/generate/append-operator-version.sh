133cc49a357dd3930ab524e284b108080c65a3a0
Pranshu Srivastava <rexagod@gmail.com>
2024-10-21 01:13:36 +0530

    fixup! fix: address OpenAPI text inconsistency for prometheus.spec.remoteWrite.sendExemplars
    Signed-off-by: Pranshu Srivastava <rexagod@gmail.com>

--- scripts/generate/append-operator-version.sh
+++ scripts/generate/append-operator-version.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-set -xuo pipefail
-
-# shellcheck disable=SC2209
-sed=sed
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  if ! command -v gsed &> /dev/null; then
-    echo 'gsed could not be found. Please install gsed.'
-    exit 1
-  fi
-  sed=gsed
+  find example/prometheus-operator-crd/ -name '*.yaml' -exec sed -i '' -e "/^    controller-gen.kubebuilder.io.version.*/a\\
+    operator.prometheus.io/version: $VERSION" {} +
+else
+  find example/prometheus-operator-crd/ -name '*.yaml' -exec sed -i "/^    controller-gen.kubebuilder.io.version.*/a\\    operator.prometheus.io/version: $VERSION" {} +
 fi
-
-find example/prometheus-operator-crd/ -name '*.yaml' -exec $sed -i "/^    controller-gen.kubebuilder.io.version.*/a\\    operator.prometheus.io/version: $VERSION" {} +
