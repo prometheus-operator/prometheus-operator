cf661a0361be05d8e028c2d7694e498d79d6ff92
Philip Gough <philip.p.gough@gmail.com>
2024-12-09 13:24:34 +0000

    Attach Authorization to config via ScrapeClass (#7154)
    * api: Add Authorization to ScrapeClass
    
    ---------
    
    Co-authored-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/apis/monitoring/v1/zz_generated.deepcopy.go
+++ pkg/apis/monitoring/v1/zz_generated.deepcopy.go
@@ -2960,6 +2960,11 @@ func (in *ScrapeClass) DeepCopyInto(out *ScrapeClass) {
 		*out = new(TLSConfig)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.Authorization != nil {
+		in, out := &in.Authorization, &out.Authorization
+		*out = new(Authorization)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.Relabelings != nil {
 		in, out := &in.Relabelings, &out.Relabelings
 		*out = make([]RelabelConfig, len(*in))
