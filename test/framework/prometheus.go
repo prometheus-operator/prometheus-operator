8b8fb3daddb6c38e3a4c8783f749566e059b0311
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2024-12-25 21:35:36 +0530

    remove unnecessary additions to framework

--- test/framework/prometheus.go
+++ test/framework/prometheus.go
@@ -664,7 +664,7 @@ func (f *Framework) WaitForDiscoveryWorking(ctx context.Context, ns, svcName, pr
 			return false, nil
 		}
 
-		working, loopErr := f.BasicQueryWorking(ctx, ns, svcName)
+		working, loopErr := f.basicQueryWorking(ctx, ns, svcName)
 		if loopErr != nil {
 			return false, loopErr
 		}
@@ -682,7 +682,7 @@ func (f *Framework) WaitForDiscoveryWorking(ctx context.Context, ns, svcName, pr
 	return nil
 }
 
-func (f *Framework) BasicQueryWorking(ctx context.Context, ns, svcName string) (bool, error) {
+func (f *Framework) basicQueryWorking(ctx context.Context, ns, svcName string) (bool, error) {
 	response, err := f.PrometheusSVCGetRequest(ctx, ns, svcName, "http", "/api/v1/query", map[string]string{"query": "up"})
 	if err != nil {
 		return false, err
