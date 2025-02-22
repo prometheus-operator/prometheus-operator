598d51e12d465d82a01e76cc8402950793f5c84d
dongjiang <dongjiang1989@126.com>
2025-02-22 10:46:13 +0800

    更新 resource_selector.go
    Co-authored-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/prometheus/resource_selector.go
+++ pkg/prometheus/resource_selector.go
@@ -1228,7 +1228,7 @@ func (rs *ResourceSelector) validateAzureSDConfigs(ctx context.Context, sc *moni
 func (rs *ResourceSelector) validateOpenStackSDConfigs(ctx context.Context, sc *monitoringv1alpha1.ScrapeConfig) error {
 	for i, config := range sc.Spec.OpenStackSDConfigs {
 		if strings.ToLower(config.Role) == "loadbalancer" && rs.version.LT(semver.MustParse("3.2.0")) {
-			return fmt.Errorf("[%d]: The `loadbalancer` role is only supported from Prometheus version 3.2.0", i)
+			return fmt.Errorf("[%d]: The %q role is only supported from Prometheus version 3.2.0", i, config.Role)
 		}
 		if config.Password != nil {
 			if _, err := rs.store.GetSecretKey(ctx, sc.GetNamespace(), *config.Password); err != nil {
