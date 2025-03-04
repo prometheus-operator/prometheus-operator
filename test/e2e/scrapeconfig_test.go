8c12e4b5ef8af434c5ee9ff4021822507fff3d2d
Dharma-09 <dharmikgangani786@gmail.com>
2025-03-04 18:39:25 -0400

    suggested changes

--- test/e2e/scrapeconfig_test.go
+++ test/e2e/scrapeconfig_test.go
@@ -3200,6 +3200,7 @@ var OpenStackSDTestCases = []scrapeCRDTestCase{
 		expectedError: true,
 	},
 }
+
 var KumaSDTestCases = []scrapeCRDTestCase{
 	{
 		name: "Valid KumaSDConfig - Required Fields",
@@ -3306,17 +3307,6 @@ var KumaSDTestCases = []scrapeCRDTestCase{
 				{
 					Server:   "http://example.com",
 					ClientID: ptr.To(""),
-				},
-			},
-		},
-		expectedError: true,
-	},
-	{
-		name: "Invalid URL",
-		scrapeConfigSpec: monitoringv1alpha1.ScrapeConfigSpec{
-			KumaSDConfigs: []monitoringv1alpha1.KumaSDConfig{
-				{
-					Server: "http://example.com",
 				},
 			},
 		},
