69a7f28ff836ca85de7cdadc0ea0dbf1ac8b8018
martin bergamasco <martin.berga@gmail.com>
2025-05-17 14:18:50 +0200

    Lint

--- pkg/prometheus/collector.go
+++ pkg/prometheus/collector.go
@@ -17,9 +17,9 @@ package prometheus
 import (
 	"github.com/prometheus/client_golang/prometheus"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/utils/ptr"
 
 	v1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
-	"k8s.io/utils/ptr"
 )
 
 var (
