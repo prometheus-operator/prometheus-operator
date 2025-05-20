ad9f1d3bfad927c2caffde27918dd429dcf38e7e
dongjiang <dongjiang1989@126.com>
2025-05-20 19:27:31 +0800

    add KAL tools
    Signed-off-by: dongjiang <dongjiang1989@126.com>

--- scripts/tools.go
+++ scripts/tools.go
@@ -33,4 +33,5 @@ import (
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+	_ "sigs.k8s.io/kube-api-linter"
 )
