58692b3473039e93cb0a61369b0f831adab5ab64
Nattapong Ekudomsuk <nuttapong_mos@hotmail.com>
2025-06-22 18:49:31 +0800

    update logic to check version

--- pkg/alertmanager/amcfg.go
+++ pkg/alertmanager/amcfg.go
@@ -466,11 +466,11 @@ func (cb *ConfigBuilder) convertGlobalConfig(ctx context.Context, in *monitoring
 		out.PagerdutyURL = &config.URL{URL: u}
 	}
 
-	telegramAllowed := cb.amVersion.GTE(semver.MustParse("0.24.0"))
-	if !telegramAllowed {
-		return nil, fmt.Errorf(`invalid syntax in global config; telegram integration is available in Alertmanager >= 0.24.0`)
-	}
 	if in.TelegramAPIURL != nil {
+		telegramAllowed := cb.amVersion.GTE(semver.MustParse("0.24.0"))
+		if !telegramAllowed {
+			return nil, fmt.Errorf(`invalid syntax in global config; telegram integration is available in Alertmanager >= 0.24.0`)
+		}
 		u, err := url.Parse(*in.TelegramAPIURL)
 		if err != nil {
 			return nil, fmt.Errorf("parse Telegram API URL: %w", err)
@@ -478,11 +478,11 @@ func (cb *ConfigBuilder) convertGlobalConfig(ctx context.Context, in *monitoring
 		out.TelegramAPIURL = &config.URL{URL: u}
 	}
 
-	webexAllowed := cb.amVersion.GTE(semver.MustParse("0.25.0"))
-	if !webexAllowed {
-		return nil, fmt.Errorf(`invalid syntax in global config; webex integration is available in Alertmanager >= 0.25.0`)
-	}
 	if in.WebexAPIURL != nil {
+		webexAllowed := cb.amVersion.GTE(semver.MustParse("0.25.0"))
+		if !webexAllowed {
+			return nil, fmt.Errorf(`invalid syntax in global config; webex integration is available in Alertmanager >= 0.25.0`)
+		}
 		u, err := url.Parse(*in.WebexAPIURL)
 		if err != nil {
 			return nil, fmt.Errorf("parse Webex API URL: %w", err)
@@ -490,11 +490,11 @@ func (cb *ConfigBuilder) convertGlobalConfig(ctx context.Context, in *monitoring
 		out.WebexAPIURL = &config.URL{URL: u}
 	}
 
-	jiraAllowed := cb.amVersion.GTE(semver.MustParse("0.28.0"))
-	if !jiraAllowed {
-		return nil, fmt.Errorf(`invalid syntax in global config; jira integration is available in Alertmanager >= 0.28.0`)
-	}
 	if in.JiraAPIURL != nil {
+		jiraAllowed := cb.amVersion.GTE(semver.MustParse("0.28.0"))
+		if !jiraAllowed {
+			return nil, fmt.Errorf(`invalid syntax in global config; jira integration is available in Alertmanager >= 0.28.0`)
+		}
 		u, err := url.Parse(*in.JiraAPIURL)
 		if err != nil {
 			return nil, fmt.Errorf("parse Jira API URL: %w", err)
