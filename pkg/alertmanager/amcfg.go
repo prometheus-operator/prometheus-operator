307eff039206a634daf96eac906cb9d4ae8d66e9
Nattapong Ekudomsuk <nuttapong_mos@hotmail.com>
2025-06-25 08:10:28 +0800

    update logic of parsing

--- pkg/alertmanager/amcfg.go
+++ pkg/alertmanager/amcfg.go
@@ -478,10 +478,8 @@ func (cb *ConfigBuilder) convertGlobalConfig(ctx context.Context, in *monitoring
 		return nil, fmt.Errorf("invalid global rocket chat config: %w", err)
 	}
 
-	if in.WebexConfig != nil {
-		if err := cb.convertGlobalWebexConfig(out, *in.WebexConfig); err != nil {
-			return nil, fmt.Errorf("invalid global webex: %w", err)
-		}
+	if err := cb.convertGlobalWebexConfig(out, in.WebexConfig); err != nil {
+		return nil, fmt.Errorf("invalid global webex config: %w", err)
 	}
 
 	return out, nil
@@ -1812,12 +1810,16 @@ func (cb *ConfigBuilder) convertGlobalRocketChatConfig(ctx context.Context, out 
 	return nil
 }
 
-func (cb *ConfigBuilder) convertGlobalWebexConfig(out *globalConfig, in monitoringv1.GlobalWebexConfig) error {
+func (cb *ConfigBuilder) convertGlobalWebexConfig(out *globalConfig, in *monitoringv1.GlobalWebexConfig) error {
+	if in == nil {
+		return nil
+	}
+
+	if cb.amVersion.LT(semver.MustParse("0.25.0")) {
+		return fmt.Errorf(`invalid syntax in global config; webex api url integration is available in Alertmanager >= 0.25.0`)
+	}
+
 	if in.APIURL != nil {
-		apiURLAllowed := cb.amVersion.GTE(semver.MustParse("0.25.0"))
-		if !apiURLAllowed {
-			return fmt.Errorf(`invalid syntax in global config; webex api url integration is available in Alertmanager >= 0.25.0`)
-		}
 		u, err := url.Parse(*in.APIURL)
 		if err != nil {
 			return fmt.Errorf("parse Webex API URL: %w", err)
