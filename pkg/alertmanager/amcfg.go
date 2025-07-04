8443cd21e4de458828f471002a32e0b375133c4f
Dhruv <136118444+slashexx@users.noreply.github.com>
2025-07-04 13:09:35 +0530

    Update pkg/alertmanager/amcfg.go
    Co-authored-by: M Viswanath Sai <110663831+mviswanathsai@users.noreply.github.com>

--- pkg/alertmanager/amcfg.go
+++ pkg/alertmanager/amcfg.go
@@ -2588,8 +2588,6 @@ func (rc *rocketChatConfig) sanitize(amVersion semver.Version, logger *slog.Logg
 	if rc.Token == nil || rc.TokenID == nil {
 		return fmt.Errorf("both token_id and token must be configured")
 	}
-	if rc.TokenID != nil && rc.Token == nil {
-		return fmt.Errorf("token must be configured")
 	}
 
 	return rc.HTTPConfig.sanitize(amVersion, logger)
