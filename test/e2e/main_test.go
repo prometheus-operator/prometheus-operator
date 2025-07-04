1b9407619f7702e15719baabfc9a08812f824bf2
slashexx <dhruvpuri.35@gmail.com>
2025-07-04 16:14:42 +0530

    add license header
    Signed-off-by: slashexx <dhruvpuri.35@gmail.com>

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -414,12 +414,12 @@ const (
 func TestGatedFeatures(t *testing.T) {
 	skipFeatureGatedTests(t)
 	testFuncs := map[string]func(t *testing.T){
-		"CreatePrometheusAgentDaemonSet":                testCreatePrometheusAgentDaemonSet,
-		"PromAgentDaemonSetResourceUpdate":              testPromAgentDaemonSetResourceUpdate,
-		"PromAgentReconcileDaemonSetResourceUpdate":     testPromAgentReconcileDaemonSetResourceUpdate,
-		"PromAgentReconcileDaemonSetResourceDelete":     testPromAgentReconcileDaemonSetResourceDelete,
-		"PrometheusAgentDaemonSetSelectPodMonitor":      testPrometheusAgentDaemonSetSelectPodMonitor,
-		"PrometheusRetentionPolicies":                   testPrometheusRetentionPolicies,
+		"CreatePrometheusAgentDaemonSet":               testCreatePrometheusAgentDaemonSet,
+		"PromAgentDaemonSetResourceUpdate":             testPromAgentDaemonSetResourceUpdate,
+		"PromAgentReconcileDaemonSetResourceUpdate":    testPromAgentReconcileDaemonSetResourceUpdate,
+		"PromAgentReconcileDaemonSetResourceDelete":    testPromAgentReconcileDaemonSetResourceDelete,
+		"PrometheusAgentDaemonSetSelectPodMonitor":     testPrometheusAgentDaemonSetSelectPodMonitor,
+		"PrometheusRetentionPolicies":                  testPrometheusRetentionPolicies,
 		"FinalizerWhenStatusForConfigResourcesEnabled": testFinalizerWhenStatusForConfigResourcesEnabled,
 		"PrometheusAgentDaemonSetCELValidations":       testPrometheusAgentDaemonSetCELValidations,
 	}
