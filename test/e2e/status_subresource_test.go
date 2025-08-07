73bbaae60c82681df5afbd07b1fe072e77b3d936
Yash Patel <yp969803@gmail.com>
2025-08-07 19:27:00 +0530

    fix: remove unnecessary reflect check for ServiceMonitor condition status in testServiceMonitorStatusSubresource

--- test/e2e/status_subresource_test.go
+++ test/e2e/status_subresource_test.go
@@ -16,7 +16,6 @@ package e2e
 
 import (
 	"context"
-	"reflect"
 	"testing"
 	"time"
 
@@ -90,5 +89,4 @@ func testServiceMonitorStatusSubresource(t *testing.T) {
 	require.Equal(t, 1, len(sm.Status.Bindings))
 	require.Equal(t, name, sm.Status.Bindings[0].Name)
 	require.Equal(t, ns, sm.Status.Bindings[0].Namespace)
-	require.Equal(t, true, reflect.DeepEqual(sm.Status.Bindings[0].Conditions[0].Status, v1.ConditionTrue), "Condition status should be true")
 }
