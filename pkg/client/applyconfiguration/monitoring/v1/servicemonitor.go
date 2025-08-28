a65906976a84e9755867ae7c35b1f6b44afc380c
Simon Pasquier <spasquie@redhat.com>
2025-08-28 16:50:16 +0200

    chore: update to Kubernetes v1.34.0
    Signed-off-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/client/applyconfiguration/monitoring/v1/servicemonitor.go
+++ pkg/client/applyconfiguration/monitoring/v1/servicemonitor.go
@@ -41,6 +41,7 @@ func ServiceMonitor(name, namespace string) *ServiceMonitorApplyConfiguration {
 	b.WithAPIVersion("monitoring.coreos.com/v1")
 	return b
 }
+func (b ServiceMonitorApplyConfiguration) IsApplyConfiguration() {}
 
 // WithKind sets the Kind field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
@@ -216,8 +217,24 @@ func (b *ServiceMonitorApplyConfiguration) WithStatus(value *ConfigResourceStatu
 	return b
 }
 
+// GetKind retrieves the value of the Kind field in the declarative configuration.
+func (b *ServiceMonitorApplyConfiguration) GetKind() *string {
+	return b.TypeMetaApplyConfiguration.Kind
+}
+
+// GetAPIVersion retrieves the value of the APIVersion field in the declarative configuration.
+func (b *ServiceMonitorApplyConfiguration) GetAPIVersion() *string {
+	return b.TypeMetaApplyConfiguration.APIVersion
+}
+
 // GetName retrieves the value of the Name field in the declarative configuration.
 func (b *ServiceMonitorApplyConfiguration) GetName() *string {
 	b.ensureObjectMetaApplyConfigurationExists()
 	return b.ObjectMetaApplyConfiguration.Name
+}
+
+// GetNamespace retrieves the value of the Namespace field in the declarative configuration.
+func (b *ServiceMonitorApplyConfiguration) GetNamespace() *string {
+	b.ensureObjectMetaApplyConfigurationExists()
+	return b.ObjectMetaApplyConfiguration.Namespace
 }
