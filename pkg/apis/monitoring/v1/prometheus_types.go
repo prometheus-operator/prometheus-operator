0b1fc913ad5c76fd5ec6b1f9df50de5cba7302ec
Nutmos <nutmos@users.noreply.github.com>
2025-09-30 15:37:04 +0700

    Apply suggestions from code review
    Update as suggested
    
    Co-authored-by: Simon Pasquier <spasquie@redhat.com>

--- pkg/apis/monitoring/v1/prometheus_types.go
+++ pkg/apis/monitoring/v1/prometheus_types.go
@@ -1912,8 +1912,8 @@ type AzureOAuth struct {
 // ManagedIdentity defines the Azure User-assigned Managed identity.
 // +k8s:openapi-gen=true
 type ManagedIdentity struct {
-	// clientId defines defines the Azure User-assigned Managed identity. to be used with Azure AD Managed Identity
-	// For Prometheus >= 3.5.0, this field is allowed to be empty to support system assigned managed identity.
+	// clientId defines the Azure User-assigned Managed identity. to be used with Azure AD Managed Identity.
+	// For Prometheus >= 3.5.0, this field is allowed to be empty to support system-assigned managed identities.
 	// +optional
 	// +kubebuilder:validation:MinLength:=1
 	ClientID *string `json:"clientId"`
