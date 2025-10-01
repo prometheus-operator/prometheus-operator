741b9228cbb54611b18fc0d8aca083d87848ce4e
Yash Patel <yp969803@gmail.com>
2025-10-01 14:49:00 +0530

    feat: add test for ScrapeConfig status subresource in gated features

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -442,6 +442,7 @@ func TestGatedFeatures(t *testing.T) {
 		"ProbeStatusSubresource":                             testProbeStatusSubresource,
 		"GarbageCollectionOfPodMonitorBinding":               testGarbageCollectionOfPodMonitorBinding,
 		"RmPodMonitorBindingDuringWorkloadDelete":            testRmPodMonitorBindingDuringWorkloadDelete,
+		"ScrapeConfigStatusSubresource":                      testScrapeConfigStatusSubresource,
 		"FinalizerForPromAgentWhenStatusForConfigResEnabled": testFinalizerForPromAgentWhenStatusForConfigResEnabled,
 	}
 
