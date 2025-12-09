prometheus-operator/prometheus-operator

commit d1ff82ef9e72ca5b447bffd4ef9cd671b46ffe2f

feat: add support for monospace in pushoverConfig (#8018)
---------

Signed-off-by: Hélia Barroso <helia_barroso@hotmail.com>

pkg/alertmanager/amcfg.go
@@ -1283,7 +1283,6 @@ func (cb *ConfigBuilder) convertPushoverConfig(ctx context.Context, in monitorin
 		URL:           in.URL,
 		URLTitle:      in.URLTitle,
 		Priority:      in.Priority,
-		HTML:          in.HTML,
 	}
 
 	if in.TTL != nil {
@@ -2447,6 +2446,7 @@ func (pdc *pagerdutyConfig) sanitize(amVersion semver.Version, logger *slog.Logg
 func (poc *pushoverConfig) sanitize(amVersion semver.Version, logger *slog.Logger) error {
 	lessThanV0_26 := amVersion.LT(semver.MustParse("0.26.0"))
 	lessThanV0_27 := amVersion.LT(semver.MustParse("0.27.0"))
+	lessThanV0_29 := amVersion.LT(semver.MustParse("0.29.0"))
 
 	if poc.UserKeyFile != "" && lessThanV0_26 {
 		msg := "'user_key_file' supported in Alertmanager >= 0.26.0 only - dropping field from pushover receiver config"
@@ -2490,6 +2490,16 @@ func (poc *pushoverConfig) sanitize(amVersion semver.Version, logger *slog.Logge
 		msg := "'device' supported in Alertmanager >= 0.26.0 only - dropping field from pushover receiver config"
 		logger.Warn(msg, "current_version", amVersion.String())
 		poc.Device = ""
+	}
+
+	if poc.Monospace != nil && *poc.Monospace && lessThanV0_29 {
+		msg := "'monospace' supported in Alertmanager >= 0.29.0 only - dropping field from pushover receiver config"
+		logger.Warn(msg, "current_version", amVersion.String())
+		*poc.Monospace = false
+	}
+
+	if poc.HTML != nil && *poc.HTML && poc.Monospace != nil && *poc.Monospace {
+		return errors.New("either monospace or html must be configured")
 	}
 
 	return poc.HTTPConfig.sanitize(amVersion, logger)

pkg/alertmanager/amcfg_test.go
@@ -5098,6 +5098,79 @@ func TestSanitizePushoverReceiverConfig(t *testing.T) {
 			},
 			expectErr: true,
 		},
+		{
+			name:           "Test pushover HTML and Monospace are mutually exclusive",
+			againstVersion: semver.Version{Major: 0, Minor: 29},
+			in: &alertmanagerConfig{
+				Receivers: []*receiver{
+					{
+						PushoverConfigs: []*pushoverConfig{
+							{
+								UserKey:   "foo",
+								Token:     "bar",
+								HTML:      ptr.To(true),
+								Monospace: ptr.To(true),
+							},
+						},
+					},
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name:           "Test pushover HTML allowed without Monospace",
+			againstVersion: semver.Version{Major: 0, Minor: 29},
+			in: &alertmanagerConfig{
+				Receivers: []*receiver{
+					{
+						PushoverConfigs: []*pushoverConfig{
+							{
+								UserKey: "foo",
+								Token:   "bar",
+								HTML:    ptr.To(true),
+							},
+						},
+					},
+				},
+			},
+			golden: "test_pushover_html_allowed_without_monospace.golden",
+		},
+		{
+			name:           "Test pushover Monospace allowed without HTML",
+			againstVersion: semver.Version{Major: 0, Minor: 29},
+			in: &alertmanagerConfig{
+				Receivers: []*receiver{
+					{
+						PushoverConfigs: []*pushoverConfig{
+							{
+								UserKey:   "foo",
+								Token:     "bar",
+								Monospace: ptr.To(true),
+							},
+						},
+					},
+				},
+			},
+			golden: "test_pushover_monospace_allowed_without_html.golden",
+		},
+		{
+			name:           "Test pushover Monospace dropped in unsupported versions",
+			againstVersion: semver.Version{Major: 0, Minor: 28},
+			in: &alertmanagerConfig{
+				Receivers: []*receiver{
+					{
+						PushoverConfigs: []*pushoverConfig{
+							{
+								UserKey:   "foo",
+								Token:     "bar",
+								Monospace: ptr.To(true),
+							},
+						},
+					},
+				},
+			},
+			golden: "test_pushover_monospace_dropped_unsupported_version.golden",
+		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			err := tc.in.sanitize(tc.againstVersion, logger)

example/prometheus-operator-crd-full/monitoring.coreos.com_alertmanagerconfigs.yaml
@@ -5295,6 +5295,11 @@ spec:
                               message defines the notification message content.
                               This is the main body text of the Pushover notification.
                             type: string
+                          monospace:
+                            description: |-
+                              monospace optional HTML/monospace formatting for the message, see https://pushover.net/api#html
+                              html and monospace formatting are mutually exclusive.
+                            type: boolean
                           priority:
                             description: |-
                               priority defines the notification priority level.
@@ -17164,6 +17169,11 @@ spec:
                               message defines the notification message content.
                               This is the main body text of the Pushover notification.
                             type: string
+                          monospace:
+                            description: |-
+                              monospace optional HTML/monospace formatting for the message, see https://pushover.net/api#html
+                              html and monospace formatting are mutually exclusive.
+                            type: boolean
                           priority:
                             description: |-
                               priority defines the notification priority level.

example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml
@@ -5296,6 +5296,11 @@ spec:
                               message defines the notification message content.
                               This is the main body text of the Pushover notification.
                             type: string
+                          monospace:
+                            description: |-
+                              monospace optional HTML/monospace formatting for the message, see https://pushover.net/api#html
+                              html and monospace formatting are mutually exclusive.
+                            type: boolean
                           priority:
                             description: |-
                               priority defines the notification priority level.

jsonnet/prometheus-operator/alertmanagerconfigs-v1beta1-crd.libsonnet
@@ -4680,6 +4680,10 @@
                             description: 'message defines the notification message content.\nThis is the main body text of the Pushover notification.',
                             type: 'string',
                           },
+                          monospace: {
+                            description: 'monospace optional HTML/monospace formatting for the message, see https://pushover.net/api#html\nhtml and monospace formatting are mutually exclusive.',
+                            type: 'boolean',
+                          },
                           priority: {
                             description: 'priority defines the notification priority level.\nSee https://pushover.net/api#priority for valid values and behavior.',
                             type: 'string',

pkg/alertmanager/operator.go
@@ -1602,6 +1602,10 @@ func checkPushoverConfigs(
 			return err
 		}
 
+		if config.HTML != nil && *config.HTML && config.Monospace != nil && *config.Monospace {
+			return errors.New("html and monospace options are mutually exclusive")
+		}
+
 		if config.Expire != "" {
 			if _, err := model.ParseDuration(config.Expire); err != nil {
 				return err

pkg/alertmanager/testdata/test_pushover_html_allowed_without_monospace.golden
@@ -1,0 +1,7 @@
+receivers:
+- name: ""
+  pushover_configs:
+  - user_key: foo
+    token: bar
+    html: true
+templates: []

pkg/alertmanager/testdata/test_pushover_monospace_allowed_without_html.golden
@@ -1,0 +1,7 @@
+receivers:
+- name: ""
+  pushover_configs:
+  - user_key: foo
+    token: bar
+    monospace: true
+templates: []

commit 574794451b5006b15b29782d64d0f21a17097b72

fix: fix Pushover HTML in Alertmanager configuration
The regression was introduced in d1ff82ef.

Closes #8152

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

pkg/alertmanager/amcfg.go
@@ -1283,6 +1283,8 @@ func (cb *ConfigBuilder) convertPushoverConfig(ctx context.Context, in monitorin
 		URL:           in.URL,
 		URLTitle:      in.URLTitle,
 		Priority:      in.Priority,
+		HTML:          in.HTML,
+		Monospace:     in.Monospace,
 	}
 
 	if in.TTL != nil {

pkg/alertmanager/amcfg_test.go
@@ -2919,6 +2919,7 @@ func TestGenerateConfig(t *testing.T) {
 								},
 								Retry:  "5m",
 								Expire: "30s",
+								HTML:   ptr.To(true),
 							}},
 						}},
 					},

pkg/alertmanager/testdata/CR_with_Pushover_Receiver.golden
@@ -13,4 +13,5 @@ receivers:
     token: tokenSecret
     retry: 5m
     expire: 30s
+    html: true
 templates: []