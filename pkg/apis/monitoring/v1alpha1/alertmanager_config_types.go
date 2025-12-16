5399a3da8bcd9ff211b4b968d9c9fb639950cafd
Rxfa <rafa3rnrn@gmail.com>
2025-12-16 23:08:41 +0000

    chore: fix OptionalField linter errors

--- pkg/apis/monitoring/v1alpha1/alertmanager_config_types.go
+++ pkg/apis/monitoring/v1alpha1/alertmanager_config_types.go
@@ -1269,6 +1269,8 @@ type TelegramConfig struct {
 	// apiURL defines the Telegram API URL, e.g. https://api.telegram.org.
 	// If not specified, the default Telegram API URL will be used.
 	// +optional
+	// nolint:kubeapilinter // v1 API stability: this violates kubeapilinter rules but is frozen for backward
+	// compatibility.
 	APIURL *URL `json:"apiURL,omitempty"`
 	// botToken defines the Telegram bot token. It is mutually exclusive with `botTokenFile`.
 	// The secret needs to be in the same namespace as the AlertmanagerConfig
