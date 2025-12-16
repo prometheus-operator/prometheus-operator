2f3ebd4c05c1f201bc8739d98bc9244eb47859bf
Nattapong Ekudomsuk <nuttapong_mos@hotmail.com>
2025-12-16 23:02:38 +0800

    remove empty string validation

--- pkg/alertmanager/validation/v1alpha1/validation.go
+++ pkg/alertmanager/validation/v1alpha1/validation.go
@@ -168,10 +168,8 @@ func validateDiscordConfigs(configs []monitoringv1alpha1.DiscordConfig) error {
 func validateRocketchatConfigs(configs []monitoringv1alpha1.RocketChatConfig) error {
 	validateRocketChatURL := func(u *monitoringv1alpha1.URL, urlName string) error {
 		if u != nil {
-			if *u != "" {
-				if _, err := validation.ValidateURL(strings.TrimSpace(string(*u))); err != nil {
-					return fmt.Errorf("failed to validate RocketChat '%s': %w", urlName, err)
-				}
+			if _, err := validation.ValidateURL(strings.TrimSpace(string(*u))); err != nil {
+				return fmt.Errorf("failed to validate RocketChat '%s': %w", urlName, err)
 			}
 		}
 
