ee3c969f76c0f8374a7da8ef5b1e09eb5dc0fdba
Nattapong Ekudomsuk <nuttapong_mos@hotmail.com>
2025-12-17 22:03:21 +0800

    update field to be separated from slack

--- pkg/alertmanager/types.go
+++ pkg/alertmanager/types.go
@@ -505,20 +505,26 @@ type mattermostConfig struct {
 }
 
 type mattermostAttachmentConfig struct {
-	Fallback   string       `yaml:"fallback,omitempty" json:"fallback,omitempty"`
-	Color      string       `yaml:"color,omitempty" json:"color,omitempty"`
-	Pretext    string       `yaml:"pretext,omitempty" json:"pretext,omitempty"`
-	Text       string       `yaml:"text,omitempty" json:"text,omitempty"`
-	AuthorName string       `yaml:"author_name,omitempty" json:"author_name,omitempty"`
-	AuthorLink string       `yaml:"author_link,omitempty" json:"author_link,omitempty"`
-	AuthorIcon string       `yaml:"author_icon,omitempty" json:"author_icon,omitempty"`
-	Title      string       `yaml:"title,omitempty" json:"title,omitempty"`
-	TitleLink  string       `yaml:"title_link,omitempty" json:"title_link,omitempty"`
-	Fields     []slackField `yaml:"fields,omitempty" json:"fields,omitempty"`
-	ThumbURL   string       `yaml:"thumb_url,omitempty" json:"thumb_url,omitempty"`
-	Footer     string       `yaml:"footer,omitempty" json:"footer,omitempty"`
-	FooterIcon string       `yaml:"footer_icon,omitempty" json:"footer_icon,omitempty"`
-	ImageURL   string       `yaml:"image_url,omitempty" json:"image_url,omitempty"`
+	Fallback   string            `yaml:"fallback,omitempty" json:"fallback,omitempty"`
+	Color      string            `yaml:"color,omitempty" json:"color,omitempty"`
+	Pretext    string            `yaml:"pretext,omitempty" json:"pretext,omitempty"`
+	Text       string            `yaml:"text,omitempty" json:"text,omitempty"`
+	AuthorName string            `yaml:"author_name,omitempty" json:"author_name,omitempty"`
+	AuthorLink string            `yaml:"author_link,omitempty" json:"author_link,omitempty"`
+	AuthorIcon string            `yaml:"author_icon,omitempty" json:"author_icon,omitempty"`
+	Title      string            `yaml:"title,omitempty" json:"title,omitempty"`
+	TitleLink  string            `yaml:"title_link,omitempty" json:"title_link,omitempty"`
+	Fields     []mattermostField `yaml:"fields,omitempty" json:"fields,omitempty"`
+	ThumbURL   string            `yaml:"thumb_url,omitempty" json:"thumb_url,omitempty"`
+	Footer     string            `yaml:"footer,omitempty" json:"footer,omitempty"`
+	FooterIcon string            `yaml:"footer_icon,omitempty" json:"footer_icon,omitempty"`
+	ImageURL   string            `yaml:"image_url,omitempty" json:"image_url,omitempty"`
+}
+
+type mattermostField struct {
+	Title string `yaml:"title,omitempty" json:"title,omitempty"`
+	Value string `yaml:"value,omitempty" json:"value,omitempty"`
+	Short bool   `yaml:"short,omitempty" json:"short,omitempty"`
 }
 
 type mattermostPropsConfig struct {
