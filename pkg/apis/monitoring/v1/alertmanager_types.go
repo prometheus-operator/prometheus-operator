prometheus-operator/prometheus-operator

commit f3bd79f142c0dd3f869b0e5f7ff9609e04d5de2d

Feat: Add Webex in Global Config (#7632)
---------

Co-authored-by: Simon Pasquier <spasquie@redhat.com>

example/prometheus-operator-crd-full/monitoring.coreos.com_alertmanagers.yaml
@@ -2251,6 +2251,17 @@ spec:
                             pattern: ^(http|https)://.+$
                             type: string
                         type: object
+                      webex:
+                        description: The default configuration for Jira.
+                        properties:
+                          apiURL:
+                            description: |-
+                              The default Webex API URL.
+
+                              It requires Alertmanager >= v0.25.0.
+                            pattern: ^(http|https)://.+$
+                            type: string
+                        type: object
                     type: object
                   name:
                     description: |-

example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
@@ -2252,6 +2252,17 @@ spec:
                             pattern: ^(http|https)://.+$
                             type: string
                         type: object
+                      webex:
+                        description: The default configuration for Jira.
+                        properties:
+                          apiURL:
+                            description: |-
+                              The default Webex API URL.
+
+                              It requires Alertmanager >= v0.25.0.
+                            pattern: ^(http|https)://.+$
+                            type: string
+                        type: object
                     type: object
                   name:
                     description: |-

pkg/apis/monitoring/v1/alertmanager_types.go
@@ -399,6 +399,9 @@ type AlertmanagerGlobalConfig struct {
 
 	// The default configuration for Rocket Chat.
 	RocketChatConfig *GlobalRocketChatConfig `json:"rocketChat,omitempty"`
+
+	// The default configuration for Jira.
+	WebexConfig *GlobalWebexConfig `json:"webex,omitempty"`
 }
 
 // AlertmanagerStatus is the most recent observed status of the Alertmanager cluster. Read-only.
@@ -555,6 +558,17 @@ type GlobalRocketChatConfig struct {
 	//
 	// +optional
 	TokenID *v1.SecretKeySelector `json:"tokenID,omitempty"`
+}
+
+// GlobalWebexConfig configures global Webex parameters.
+// See https://prometheus.io/docs/alerting/latest/configuration/#configuration-file
+type GlobalWebexConfig struct {
+	// The default Webex API URL.
+	//
+	// It requires Alertmanager >= v0.25.0.
+	//
+	// +optional
+	APIURL *URL `json:"apiURL,omitempty"`
 }
 
 // HostPort represents a "host:port" network address.

bundle.yaml
@@ -13053,6 +13053,17 @@ spec:
                             pattern: ^(http|https)://.+$
                             type: string
                         type: object
+                      webex:
+                        description: The default configuration for Jira.
+                        properties:
+                          apiURL:
+                            description: |-
+                              The default Webex API URL.
+
+                              It requires Alertmanager >= v0.25.0.
+                            pattern: ^(http|https)://.+$
+                            type: string
+                        type: object
                     type: object
                   name:
                     description: |-

pkg/alertmanager/amcfg.go
@@ -478,6 +478,10 @@ func (cb *ConfigBuilder) convertGlobalConfig(ctx context.Context, in *monitoring
 		return nil, fmt.Errorf("invalid global rocket chat config: %w", err)
 	}
 
+	if err := cb.convertGlobalWebexConfig(out, in.WebexConfig); err != nil {
+		return nil, fmt.Errorf("invalid global webex config: %w", err)
+	}
+
 	return out, nil
 }
 
@@ -1801,6 +1805,26 @@ func (cb *ConfigBuilder) convertGlobalRocketChatConfig(ctx context.Context, out 
 			return fmt.Errorf("failed to get Rocket Chat Token ID: %w", err)
 		}
 		out.RocketChatTokenID = tokenID
+	}
+
+	return nil
+}
+
+func (cb *ConfigBuilder) convertGlobalWebexConfig(out *globalConfig, in *monitoringv1.GlobalWebexConfig) error {
+	if in == nil {
+		return nil
+	}
+
+	if cb.amVersion.LT(semver.MustParse("0.25.0")) {
+		return fmt.Errorf(`webex integration requires Alertmanager >= 0.25.0`)
+	}
+
+	if in.APIURL != nil {
+		u, err := url.Parse(string(*in.APIURL))
+		if err != nil {
+			return fmt.Errorf("parse Webex API URL: %w", err)
+		}
+		out.WebexAPIURL = &config.URL{URL: u}
 	}
 
 	return nil

pkg/alertmanager/amcfg_test.go
@@ -94,6 +94,9 @@ func TestInitializeFromAlertmanagerConfig(t *testing.T) {
 	rocketChatAPIURL := "https://rocketchat.example.com"
 	invalidRocketChatAPIURL := "://rocketchat.example.com"
 
+	webexAPIURL := "https://webex.example.com"
+	invalidWebexAPIURL := "://webex.example.com"
+
 	tests := []struct {
 		name            string
 		amVersion       *semver.Version
@@ -1340,6 +1343,117 @@ func TestInitializeFromAlertmanagerConfig(t *testing.T) {
 						},
 						Key: "token_id",
 					},
+				},
+			},
+			amConfig: &monitoringv1alpha1.AlertmanagerConfig{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "global-config",
+					Namespace: "mynamespace",
+				},
+				Spec: monitoringv1alpha1.AlertmanagerConfigSpec{
+					Receivers: []monitoringv1alpha1.Receiver{
+						{
+							Name: "null",
+						},
+						{
+							Name: "myreceiver",
+						},
+					},
+					Route: &monitoringv1alpha1.Route{
+						Receiver: "null",
+						Routes: []apiextensionsv1.JSON{
+							{
+								Raw: myrouteJSON,
+							},
+						},
+					},
+				},
+			},
+			matcherStrategy: monitoringv1.AlertmanagerConfigMatcherStrategy{
+				Type: "OnNamespace",
+			},
+			wantErr: true,
+		},
+		{
+			name:      "valid global config webex api url",
+			amVersion: &version28,
+			globalConfig: &monitoringv1.AlertmanagerGlobalConfig{
+				WebexConfig: &monitoringv1.GlobalWebexConfig{
+					APIURL: ptr.To(monitoringv1.URL(webexAPIURL)),
+				},
+			},
+			amConfig: &monitoringv1alpha1.AlertmanagerConfig{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "global-config",
+					Namespace: "mynamespace",
+				},
+				Spec: monitoringv1alpha1.AlertmanagerConfigSpec{
+					Receivers: []monitoringv1alpha1.Receiver{
+						{
+							Name: "null",
+						},
+						{
+							Name: "myreceiver",
+						},
+					},
+					Route: &monitoringv1alpha1.Route{
+						Receiver: "null",
+						Routes: []apiextensionsv1.JSON{
+							{
+								Raw: myrouteJSON,
+							},
+						},
+					},
+				},
+			},
+			matcherStrategy: monitoringv1.AlertmanagerConfigMatcherStrategy{
+				Type: "OnNamespace",
+			},
+			golden: "valid_global_config_with_Webex_API_URL.golden",
+		},
+		{
+			name:      "invalid global config webex api url",
+			amVersion: &version28,
+			globalConfig: &monitoringv1.AlertmanagerGlobalConfig{
+				WebexConfig: &monitoringv1.GlobalWebexConfig{
+					APIURL: ptr.To(monitoringv1.URL(invalidWebexAPIURL)),
+				},
+			},
+			amConfig: &monitoringv1alpha1.AlertmanagerConfig{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "global-config",
+					Namespace: "mynamespace",
+				},
+				Spec: monitoringv1alpha1.AlertmanagerConfigSpec{
+					Receivers: []monitoringv1alpha1.Receiver{
+						{
+							Name: "null",
+						},
+						{
+							Name: "myreceiver",
+						},
+					},
+					Route: &monitoringv1alpha1.Route{
+						Receiver: "null",
+						Routes: []apiextensionsv1.JSON{
+							{
+								Raw: myrouteJSON,
+							},
+						},
+					},
+				},
+			},
+			matcherStrategy: monitoringv1.AlertmanagerConfigMatcherStrategy{
+				Type: "OnNamespace",
+			},
+			wantErr: true,
+		},
+		{
+			name:      "invalid global config webex api url version not supported",
+			amVersion: &version24,
+			globalConfig: &monitoringv1.AlertmanagerGlobalConfig{
+				WebexConfig: &monitoringv1.GlobalWebexConfig{
+					APIURL: ptr.To(monitoringv1.URL(webexAPIURL)),
 				},
 			},
 			amConfig: &monitoringv1alpha1.AlertmanagerConfig{

pkg/alertmanager/testdata/valid_global_config_with_Webex_API_URL.golden
@@ -1,0 +1,13 @@
+global:
+  webex_api_url: https://webex.example.com
+route:
+  receiver: mynamespace/global-config/null
+  routes:
+  - receiver: mynamespace/global-config/myreceiver
+    matchers:
+    - mykey="myvalue"
+    - mykey1="myvalue1"
+receivers:
+- name: mynamespace/global-config/null
+- name: mynamespace/global-config/myreceiver
+templates: []

pkg/apis/monitoring/v1/zz_generated.deepcopy.go
@@ -257,6 +257,11 @@ func (in *AlertmanagerGlobalConfig) DeepCopyInto(out *AlertmanagerGlobalConfig) 
 		*out = new(GlobalRocketChatConfig)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.WebexConfig != nil {
+		in, out := &in.WebexConfig, &out.WebexConfig
+		*out = new(GlobalWebexConfig)
+		(*in).DeepCopyInto(*out)
+	}
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new AlertmanagerGlobalConfig.
@@ -1536,6 +1541,26 @@ func (in *GlobalTelegramConfig) DeepCopy() *GlobalTelegramConfig {
 		return nil
 	}
 	out := new(GlobalTelegramConfig)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *GlobalWebexConfig) DeepCopyInto(out *GlobalWebexConfig) {
+	*out = *in
+	if in.APIURL != nil {
+		in, out := &in.APIURL, &out.APIURL
+		*out = new(URL)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new GlobalWebexConfig.
+func (in *GlobalWebexConfig) DeepCopy() *GlobalWebexConfig {
+	if in == nil {
+		return nil
+	}
+	out := new(GlobalWebexConfig)
 	in.DeepCopyInto(out)
 	return out
 }

commit b45259dc4f91affc0b197044b993b4f5393bf343

docs(alertmanager_types.go): fix misleading doc for WebexConfig
issue was introduced in f3bd79f142c0dd3f869b0e5f7ff9609e04d5de2d

example/prometheus-operator-crd-full/monitoring.coreos.com_alertmanagers.yaml
@@ -2318,7 +2318,7 @@ spec:
                             type: string
                         type: object
                       webex:
-                        description: webex defines the default configuration for Jira.
+                        description: webex defines the default configuration for Webex.
                         properties:
                           apiURL:
                             description: |-

example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
@@ -2319,7 +2319,7 @@ spec:
                             type: string
                         type: object
                       webex:
-                        description: webex defines the default configuration for Jira.
+                        description: webex defines the default configuration for Webex.
                         properties:
                           apiURL:
                             description: |-

pkg/apis/monitoring/v1/alertmanager_types.go
@@ -519,7 +519,7 @@ type AlertmanagerGlobalConfig struct {
 	// +optional
 	RocketChatConfig *GlobalRocketChatConfig `json:"rocketChat,omitempty"`
 
-	// webex defines the default configuration for Jira.
+	// webex defines the default configuration for Webex.
 	// +optional
 	WebexConfig *GlobalWebexConfig `json:"webex,omitempty"`
 