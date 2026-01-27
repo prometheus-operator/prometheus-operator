2ad3720a85e41528ad717e5df2fd71c143726804
Billy <billydevbusiness@gmail.com>
2026-01-27 20:03:28 +0530

    tests: add unit tests for stuck StatefulSet repair logic

--- pkg/alertmanager/statefulset_test.go
+++ pkg/alertmanager/statefulset_test.go
@@ -1570,7 +1570,7 @@ func TestStatefulSetUpdateStrategy(t *testing.T) {
 		},
 		{
 			updateStrategy: &monitoringv1.StatefulSetUpdateStrategy{
-				Type: monitoringv1.RollingUpdateStatefulSetStrategyType,
+				Type: appsv1.RollingUpdateStatefulSetStrategyType,
 			},
 			exp: appsv1.StatefulSetUpdateStrategy{
 				Type: appsv1.RollingUpdateStatefulSetStrategyType,
@@ -1578,7 +1578,7 @@ func TestStatefulSetUpdateStrategy(t *testing.T) {
 		},
 		{
 			updateStrategy: &monitoringv1.StatefulSetUpdateStrategy{
-				Type: monitoringv1.RollingUpdateStatefulSetStrategyType,
+				Type: appsv1.RollingUpdateStatefulSetStrategyType,
 				RollingUpdate: &monitoringv1.RollingUpdateStatefulSetStrategy{
 					MaxUnavailable: ptr.To(intstr.FromInt(1)),
 				},
@@ -1592,7 +1592,7 @@ func TestStatefulSetUpdateStrategy(t *testing.T) {
 		},
 		{
 			updateStrategy: &monitoringv1.StatefulSetUpdateStrategy{
-				Type: monitoringv1.OnDeleteStatefulSetStrategyType,
+				Type: appsv1.OnDeleteStatefulSetStrategyType,
 			},
 			exp: appsv1.StatefulSetUpdateStrategy{
 				Type: appsv1.OnDeleteStatefulSetStrategyType,
