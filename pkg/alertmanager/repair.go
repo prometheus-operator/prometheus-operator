f1046ea14e1cd0267b9013ef137276721638f7fe
Billy <billydevbusiness@gmail.com>
2026-01-27 18:55:57 +0530

    pkg: integrate repair policy into operator controllers

---
+++ pkg/alertmanager/repair.go
@@ -0,0 +1,29 @@
+// Copyright 2025 The prometheus-operator Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package alertmanager
+
+import (
+	"context"
+	"log/slog"
+
+	appsv1 "k8s.io/api/apps/v1"
+
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	"github.com/prometheus-operator/prometheus-operator/pkg/operator"
+)
+
+func (c *Operator) resolveStuckStatefulSet(ctx context.Context, logger *slog.Logger, _ *monitoringv1.Alertmanager, sset *appsv1.StatefulSet) error {
+	return operator.ResolveStuckStatefulSet(ctx, logger, c.kclient, sset, c.config.RepairPolicy)
+}
