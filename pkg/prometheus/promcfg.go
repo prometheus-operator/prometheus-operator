ea27a4697237115e382577420e181d5eaf93edbd
Sanchit2662 <sanchit2662@gmail.com>
2026-03-06 20:40:28 +0530

    Add missing OAuth2 field to IonosSDConfig generation
    Signed-off-by: Sanchit2662 <sanchit2662@gmail.com>

--- pkg/prometheus/promcfg.go
+++ pkg/prometheus/promcfg.go
@@ -4763,6 +4763,7 @@ func (cg *ConfigGenerator) generateScrapeConfig(
 		configs := make([][]yaml.MapItem, len(sc.Spec.IonosSDConfigs))
 		for i, config := range sc.Spec.IonosSDConfigs {
 			configs[i] = cg.addSafeAuthorizationToYaml(configs[i], s, &config.Authorization)
+			configs[i] = cg.addOAuth2ToYaml(configs[i], s, config.OAuth2)
 			configs[i] = cg.addProxyConfigtoYaml(configs[i], s, config.ProxyConfig)
 			configs[i] = cg.addSafeTLStoYaml(configs[i], s, config.TLSConfig)
 
