450af4d0f0e39855f6285bb5da9bfeb1dd5b7d6b
Nattapong Ekudomsuk <nuttapong_mos@hotmail.com>
2026-04-22 21:09:35 +0800

    alertmanager: move check method
    change to nil check instead
    
    Signed-off-by: Nattapong Ekudomsuk <nuttapong_mos@hotmail.com>

--- pkg/alertmanager/amcfg.go
+++ pkg/alertmanager/amcfg.go
@@ -2931,7 +2931,7 @@ func (whc *webhookConfig) sanitize(amVersion semver.Version, logger *slog.Logger
 		whc.Timeout = nil
 	}
 
-	if len(whc.Payload) != 0 && amVersion.LT(semver.MustParse("0.32.0")) {
+	if whc.Payload != nil && amVersion.LT(semver.MustParse("0.32.0")) {
 		msg := "'payload' supported in Alertmanager >= 0.32.0 only - dropping field from provided config"
 		logger.Warn(msg, "current_version", amVersion.String())
 		whc.Payload = nil
