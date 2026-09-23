1e6981c7a20b13b2efd5ef5fa186ce3812919640
Nattapong Ekudomsuk <nuttapong_mos@hotmail.com>
2026-09-23 12:38:30 +0700

    alertmanager: update new line
    to align with linter

--- pkg/alertmanager/validation/v1alpha1/validation.go
+++ pkg/alertmanager/validation/v1alpha1/validation.go
@@ -121,7 +121,6 @@ func validateReceivers(receivers []monitoringv1alpha1.Receiver) (map[string]stru
 		if err := validateMattermostConfigs(receiver.MattermostConfigs); err != nil {
 			return nil, fmt.Errorf("failed to validate 'mattermostConfig' - receiver %s: %w", receiver.Name, err)
 		}
-
 	}
 
 	return receiverNames, nil
@@ -569,7 +568,6 @@ func validateMSTeamsV2Configs(configs []monitoringv1alpha1.MSTeamsV2Config) erro
 
 func validateMattermostConfigs(configs []monitoringv1alpha1.MattermostConfig) error {
 	for i, config := range configs {
-
 		if err := validation.ValidateTemplateURLPtr(config.IconURL); err != nil {
 			return fmt.Errorf("[%d]: iconURL: %w", i, err)
 		}
